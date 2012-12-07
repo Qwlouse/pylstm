9e65113cf46d87c7142464f3339df304e69c9a67
Marijn Stollenga <marijn@idsia.ch>
2012-12-07 14:21:44 +0100

    test equals

--- test/matrix/TestMatrix.cpp
+++ test/matrix/TestMatrix.cpp
@@ -4,8 +4,15 @@
 
 #include "Config.h"
 
+#include "matrix/matrix_cpu.h"
+#include "matrix/matrix_operation_cpu.h"
+
 TEST(TestMatrix, check_if_one_is_true)
 {
-    ASSERT_TRUE(1);
+  ASSERT_TRUE(equals(MatrixCPU({{2.,3.,5.},{3.,4.,55.}}), MatrixCPU({{2.,3.,5.},{3.,4.,55.}})));
+
+  ASSERT_TRUE(!equals(MatrixCPU({{2.,3.,4.},{3.,4.,55.}}), MatrixCPU({{2.,3.,5.},{3.,4.,55.}})));
+  ASSERT_TRUE(!equals(MatrixCPU({{2.,3.,4.},{3.,4.,55.}}), MatrixCPU({{2.,3.},{3.,4.}})));
+
 }
 
