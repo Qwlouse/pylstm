285d10148a724af25e210d1b2458298f36072a8b
Klaus Greff <klaus@idsia.ch>
2013-01-09 12:59:58 +0100

    added test directory for pylstm and added to build process

--- cmake/ReplicatePythonSourceTree.cmake
+++ cmake/ReplicatePythonSourceTree.cmake
@@ -6,3 +6,6 @@ ENDIF (EXISTS "setup.py")
 
 FILE(COPY src/pylstm DESTINATION "${CMAKE_ARGV3}"
      FILES_MATCHING PATTERN "*.py")
+
+FILE(COPY test/pylstm/ DESTINATION "${CMAKE_ARGV3}/pylstm/test"
+     FILES_MATCHING PATTERN "*.py")
