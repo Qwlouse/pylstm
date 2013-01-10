166d0098fab6fc395e68f5096d1a47a21e2a8e62
Klaus Greff <klaus@idsia.ch>
2013-01-10 14:34:40 +0100

    changed netbuilder interface a little

--- src/pylstm/netbuilder.py
+++ src/pylstm/netbuilder.py
@@ -4,57 +4,59 @@
 class InvalidArchitectureError(RuntimeError):
     pass
 
+def create_ConstructionLayer(LayerType):
+    class ConstructionLayer(object):
+        def __init__(self, out_size, **layer_kwargs):
+            self.out_size = out_size
+            self.targets = []
+            self.sources = []
+            self.LayerType = LayerType
+            self.layer_kwargs = layer_kwargs
+            self.depth = None
+            self.traversing = False
 
-class ConstructionLayer(object):
-    def __init__(self, LayerType, out_size, **layer_kwargs):
-        self.out_size = out_size
-        self.targets = []
-        self.sources = []
-        self.LayerType = LayerType
-        self.layer_kwargs = layer_kwargs
-        self.depth = None
-        self.traversing = False
+        def instantiate(self):
+            return self.LayerType(self.get_input_size(),
+                self.out_size, **self.layer_kwargs)
 
-    def instantiate(self):
-        return self.LayerType(self.get_input_size(),
-            self.out_size, **self.layer_kwargs)
+        def get_input_size(self):
+            return sum(s.out_size for s in self.sources)
 
-    def get_input_size(self):
-        return sum(s.out_size for s in self.sources)
+        def get_depth(self):
+            if self.depth is None:
+                self.depth = float('inf') # marker for "get_depth" in progress
+                self.depth = max(s.get_depth() for s in self.sources) + 1
+            return self.depth
 
-    def get_depth(self):
-        if self.depth is None:
-            self.depth = float('inf') # marker for "get_depth" in progress
-            self.depth = max(s.get_depth() for s in self.sources) + 1
-        return self.depth
+        def traverse_targets_tree(self):
+            if self.traversing:
+                raise InvalidArchitectureError("Circle in Network")
+            self.traversing = True
+            yield self
+            for target in self.targets:
+                for t in target.traverse_targets_tree():
+                    yield t
+            self.traversing = False
 
-    def traverse_targets_tree(self):
-        if self.traversing:
-            raise InvalidArchitectureError("Circle in Network")
-        self.traversing = True
-        yield self
-        for target in self.targets:
-            for t in target.traverse_targets_tree():
-                yield t
-        self.traversing = False
+        def _add_source(self, other):
+            self.sources.append(other)
 
-    def _add_source(self, other):
-        self.sources.append(other)
+        def __rshift__(self, other):
+            self.targets.append(other)
+            other._add_source(self)
+            return other
+    return ConstructionLayer
 
-    def __rshift__(self, other):
-        self.targets.append(other)
-        other._add_source(self)
-        return other
-
+DummyLayer = create_ConstructionLayer(None)
 
 class NetworkBuilder():
     def __init__(self):
         self.input_layer = None
-        self.output = ConstructionLayer(None, 0)
+        self.output = DummyLayer(0)
 
     def input(self, size=None):
         if size :
-            self.input_layer = ConstructionLayer(None, size)
+            self.input_layer = DummyLayer(size)
         return self.input_layer
 
     def get_sorted_layers(self):
@@ -90,3 +92,7 @@ class NetworkBuilder():
             raise InvalidArchitectureError()
         # WIP...
 
+
+################################################################################
+
+#LstmLayer = create_ConstructionLayer(layers.Lstm)
--- test/pylstm/test_netbuilder.py
+++ test/pylstm/test_netbuilder.py
@@ -2,27 +2,29 @@
 # coding=utf-8
 
 import unittest
-from pylstm.netbuilder import NetworkBuilder, InvalidArchitectureError, ConstructionLayer
+from pylstm.netbuilder import NetworkBuilder, InvalidArchitectureError, create_ConstructionLayer
 
 class Foo(): pass
+
+FooLayer = create_ConstructionLayer(Foo)
 
 #noinspection PyStatementEffect
 class ConstructionLayerTests(unittest.TestCase):
     def test_constructor(self):
-        cl = ConstructionLayer(Foo, 7)
+        cl = FooLayer(7)
         self.assertEqual(cl.out_size, 7)
         self.assertEqual(cl.LayerType, Foo)
 
     def test_connecting_two_layers(self):
-        cl1 = ConstructionLayer(Foo, 3)
-        cl2 = ConstructionLayer(Foo, 4)
+        cl1 = FooLayer(3)
+        cl2 = FooLayer(4)
         cl1 >> cl2
         self.assertIn(cl1, cl2.sources)
         self.assertIn(cl2, cl1.targets)
         self.assertEqual(cl2.get_input_size(), 3)
 
     def test_connect_multiple_targets(self):
-        l0, l1, l2, l3 = [ConstructionLayer(Foo, 10) for _ in range(4)]
+        l0, l1, l2, l3 = [FooLayer(10) for _ in range(4)]
         l0 >> l1
         l0 >> l2
         l0 >> l3
@@ -31,7 +33,7 @@ class ConstructionLayerTests(unittest.TestCase):
         self.assertIn(l3, l0.targets)
 
     def test_connect_multiple_sources(self):
-        l0, l1, l2, l3 = [ConstructionLayer(Foo, 10) for _ in range(4)]
+        l0, l1, l2, l3 = [FooLayer(10) for _ in range(4)]
         l1 >> l0
         l2 >> l0
         l3 >> l0
@@ -41,20 +43,20 @@ class ConstructionLayerTests(unittest.TestCase):
 
 
     def test_get_depth_linear_architecture(self):
-        layers = [ConstructionLayer(Foo, 10) for _ in range(5)]
+        layers = [FooLayer(10) for _ in range(5)]
         for i in range(4):
             layers[i] >> layers[i+1]
         layers[0].depth = 0
         self.assertListEqual([l.get_depth() for l in layers], list(range(5)))
 
     def test_traverse_targets_tree_linear_architecture(self):
-        layers = [ConstructionLayer(Foo, 10) for _ in range(5)]
+        layers = [FooLayer(10) for _ in range(5)]
         for i in range(4):
             layers[i] >> layers[i+1]
         self.assertListEqual(list(layers[0].traverse_targets_tree()), layers)
 
     def test_get_depth_more_complicated(self):
-        layers = [ConstructionLayer(Foo, 10) for _ in range(5)]
+        layers = [FooLayer(10) for _ in range(5)]
         l0, l1, l2, l3, l4 = layers
         l0 >> l1 >> l2 >> l3
         l0 >> l4 >> l3
@@ -62,14 +64,14 @@ class ConstructionLayerTests(unittest.TestCase):
         self.assertListEqual([l.get_depth() for l in layers], [0, 1, 2, 3, 1])
 
     def test_traverse_more_complicated(self):
-        layers = [ConstructionLayer(Foo, 10) for _ in range(5)]
+        layers = [FooLayer(10) for _ in range(5)]
         l0, l1, l2, l3, l4 = layers
         l0 >> l1 >> l2 >> l3
         l0 >> l4 >> l3
         self.assertSetEqual(set(layers), set(l0.traverse_targets_tree()))
 
     def test_traverse_circle_raises_error(self):
-        l0, l1, l2 = [ConstructionLayer(Foo, 10) for _ in range(3)]
+        l0, l1, l2 = [FooLayer(10) for _ in range(3)]
         l0 >> l1 >> l2 >> l0
         try:
             list(l0.traverse_targets_tree())
@@ -92,9 +94,9 @@ class NetBuilderTests(unittest.TestCase):
         self.netb.build()
 
     def test_topological_sort(self):
-        cl = lambda : ConstructionLayer(Foo, 2)
-        self.netb.input(3) >> cl() >> cl() >> cl() >> self.netb.output
-        self.netb.input() >> cl() >> self.netb.output
+        fl = FooLayer
+        self.netb.input(3) >> fl(2) >> fl(2) >> fl(2) >> self.netb.output
+        self.netb.input() >> fl(2) >> self.netb.output
         self.netb.input() >> self.netb.output
         layers = self.netb.get_sorted_layers()
         current_depth = 0
