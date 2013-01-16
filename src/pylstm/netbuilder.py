2e5bd3792e8a0db64db7bb866ae3e218a2ee9c8d
Klaus Greff <klaus@idsia.ch>
2013-01-16 19:19:07 +0100

    new version of build that uses BufferManagers

--- src/pylstm/netbuilder.py
+++ src/pylstm/netbuilder.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # coding=utf-8
 import pylstm_wrapper
+from collections import OrderedDict
 
 class InvalidArchitectureError(RuntimeError):
     pass
 
 def create_ConstructionLayer(LayerType):
     class ConstructionLayer(object):
-        def __init__(self, out_size, **layer_kwargs):
+        def __init__(self, out_size, name = None, **layer_kwargs):
             self.out_size = out_size
+            self.name = name
             self.targets = []
             self.sources = []
             self.LayerType = LayerType
@@ -46,6 +48,10 @@ def create_ConstructionLayer(LayerType):
             self.targets.append(other)
             other._add_source(self)
             return other
+
+        def get_name(self):
+            return self.name or self.LayerType.__name__
+
     return ConstructionLayer
 
 DummyLayer = create_ConstructionLayer(None)
@@ -66,6 +72,49 @@ class Network(object):
         Y = self.forward_buffers[-1]
         return Y
 
+class BufferManager(object):
+    def __init__(self):
+        self.size_getters = {}
+        self.view_factories = {}
+        self.buffer = None
+        self.views = {}
+
+    def add(self, name, size_getter, view_factories):
+        self.buffer = None
+        self.size_getters[name] = size_getter
+        self.view_factories[name] = view_factories
+
+    def calculate_size(self, slice_count=1, batch_count=1):
+        return sum(sg(slice_count, batch_count) for sg in self.size_getters)
+
+    def initialize_buffer(self, slice_count=1, batch_count=1):
+        total_size = self.calculate_size(slice_count, batch_count)
+        self.buffer = pylstm_wrapper.MatrixView(
+            pylstm_wrapper.MatrixCPU(total_size, 1, 1))
+
+    def lay_out_views(self, slice_count=1, batch_count=1):
+        param_start = 0
+        for name, vfs in self.view_factories.items():
+            param_size = self.size_getters[name](slice_count, batch_count)
+            param_view = self.buffer.slice(param_start, param_start + param_size)
+            param_start += param_size
+            self.views[name] = [vf(param_view, slice_count, batch_count) for vf in vfs]
+
+    def get_buffer(self, name, slice_count=1, batch_count=1):
+        if not self.buffer:
+            self.initialize_buffer(slice_count, batch_count)
+            self.lay_out_views(slice_count, batch_count)
+        else:
+            # check sizes
+            new_size = self.calculate_size(slice_count, batch_count)
+            if new_size > self.buffer.get_slice_count():
+                self.initialize_buffer(slice_count, batch_count)
+            if new_size < self.buffer.get_slice_count():
+                self.lay_out_views(slice_count, batch_count)
+        return self.views[name]
+
+
+
 class NetworkBuilder():
     def __init__(self):
         self.input_layer = None
@@ -77,6 +126,8 @@ class NetworkBuilder():
         return self.input_layer
 
     def get_sorted_layers(self):
+        if not self.input_layer:
+            raise InvalidArchitectureError("Empty")
         # gather all the layers
         layers = set(self.input_layer.traverse_targets_tree())
         # sort them by depth
@@ -117,7 +168,7 @@ class NetworkBuilder():
         assert cLayers[-1] is self.output
         layers = [cl.instantiate() for cl in cLayers[1:-1]] # without in and out layer
 
-        total_param_size = sum(l.get_param_size() for l in layers)
+        total_param_size = sum(l.get_param_size() for l in layers) or 1
         param_buffer = self.create_buffer(total_param_size)
         internal_buffers = []
         in_buffer = self.create_buffer(self.input_layer.out_size)
@@ -144,6 +195,48 @@ class NetworkBuilder():
             ))
             current_in_buffer = out_buffer
         return Network(layers, views, param_buffer, internal_buffers, forward_buffers)
+
+
+    def get_named_layers(self):
+        # instantiate all the layers with names
+        cLayers = self.get_sorted_layers()
+        assert cLayers[0] is self.input_layer
+        assert cLayers[-1] is self.output
+        layers = OrderedDict()
+        for l in cLayers[1:-1]:
+            layer = l.instantiate()
+            name = l.get_name()
+            # ensure unique name
+            if name in layers:
+                basename = name
+                idx = 1
+                while name in layers:
+                    name = basename + "_%d"%idx
+                    idx += 1
+            l.name = name
+            layers[l.name] = layer
+        return layers, cLayers
+
+    def build2(self):
+        layers, cLayers = self.get_named_layers()
+
+        weight_manager = BufferManager()
+        for name, l in layers.items():
+            weight_manager.add(name, l.get_param_size, [l.create_param_view])
+
+        intern_manager = BufferManager()
+        for name, l in layers.items():
+            intern_manager.add(name, l.get_internal_state_size, [l.create_internal_view])
+
+        output_manager = BufferManager()
+        for layer in cLayers.items():
+            lset, rset = self.get_forward_closure(layer)
+            assert len(lset)==len(rset)==1, "Complicated Architectures not supported yet"
+            name = lset.pop().name
+            l = layers[name]
+            r = layers[rset.pop().name]
+            output_manager.add(name, l.get_output_buffer_size, [l.create_output_view, r.create_input_view])
+
 
 
         # WIP...
