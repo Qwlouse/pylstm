44571aaa312ee8829c1c4d165432a8093e4000cb
Klaus Greff <klaus@idsia.ch>
2013-02-06 16:38:35 +0100

    fixed check_deltas by resetting internal state

--- src/pylstm/utils.py
+++ src/pylstm/utils.py
@@ -10,17 +10,19 @@ from pylstm.netbuilder import NetworkBuilder
 from scipy.optimize import approx_fprime
 import numpy as np
 
-
+rnd = np.random.RandomState(12345)
 
 def check_deltas(net, X = None):
     def f(x):
+        net.clear_internal_state()
         out = net.forward_pass(x.reshape(1,1,-1)).as_array()
+
         return .5*np.sum(out**2)
 
     if X is None:
-        X = np.random.randn(net.get_input_size())
+        X = rnd.randn(net.get_input_size())
 
-    delta_approx = approx_fprime(X, f, 1e-7)
+    delta_approx = approx_fprime(X, f, 1e-1)
     out = net.forward_pass(X.reshape(1,1,-1)).as_array()
     delta_calc = net.backward_pass(out).as_array()
     return np.sum((delta_approx - delta_calc)**2), delta_calc, delta_approx
@@ -31,7 +33,8 @@ if __name__ == "__main__":
     netb  = NetworkBuilder()
     netb.input(5) >> LstmLayer(7) >> netb.output
     net = netb.build()
-    weights = np.random.randn(net.get_param_size())
+
+    weights = rnd.randn(net.get_param_size())
     # and set them as the parameter buffer
     net.set_param_buffer(weights)
 
