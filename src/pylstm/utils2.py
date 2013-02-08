3f2fd2c5c39577b0a2a9c3dd6e513fbc8455d3a4
sohrob <sohrob@gmail.com>
2013-02-08 15:11:16 +0100

    new utils file

---
+++ src/pylstm/utils2.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# coding=utf-8
+
+from __future__ import division, print_function, unicode_literals
+import sys
+sys.path.append('.')
+sys.path.append('..')
+from pylstm.layers import NpFwdLayer, LstmLayer
+from pylstm.netbuilder import NetworkBuilder
+from scipy.optimize import approx_fprime
+import numpy as np
+
+rnd = np.random.RandomState(12345)
+
+def check_deltas(net, X = None):
+    def f(x):
+        net.clear_internal_state()
+        out = net.forward_pass(x.reshape(1,1,-1)).as_array()
+
+        return .5*np.sum(out**2)
+
+    if X is None:
+        X = rnd.randn(net.get_input_size())
+
+    delta_approx = approx_fprime(X, f, 1e-7)
+    out = net.forward_pass(X.reshape(2,1,-1)).as_array()
+    delta_calc = net.backward_pass(out).as_array()
+    return np.sum((delta_approx - delta_calc)**2), delta_calc, delta_approx
+
+
+def check_gradient(net, X = None):
+    
+    X = rnd.randn(net.get_input_size()*1)
+    timesteps = 1
+    def f(w):
+
+        net.clear_internal_state()
+        net.set_param_buffer(w)
+        out = net.forward_pass(X.reshape(timesteps,1,-1)).as_array()
+        
+        return .5*np.sum(out**2)
+
+    weights = rnd.randn(net.get_param_size())
+    #print(weights)
+
+    tmpweights = weights
+    tmpweights[0] += 1e-7
+    #print(tmpweights)
+
+    
+
+    out = net.forward_pass(X.reshape(timesteps,1,-1)).as_array()
+    #print(out) 
+    err1 = out.copy()
+  
+    net.clear_internal_state()
+    net.set_param_buffer(tmpweights)
+    newout = net.forward_pass(X.reshape(timesteps,1,-1)).as_array()
+    
+    #print(newout)
+    err2 = newout.copy()
+    print("error1: ", err1, "type: ", type(err1), " ",err1.shape)
+    print("error2: ", err2, "type: ", type(err2), " ", err2.shape) 
+  
+    differrors = err1 - err2
+    print("differr", differrors)
+
+    #print(err1-err2)
+    
+
+    grad_approx = approx_fprime(weights, f, 1e-7)
+    
+    net.clear_internal_state()
+    net.set_param_buffer(weights)
+    out = net.forward_pass(X.reshape(timesteps,1,-1)).as_array()
+    #print(out)
+    delta_calc = net.backward_pass(out).as_array()
+    grad_calc = net.calc_gradient().as_array()
+    
+    return np.sum((grad_approx - grad_calc.squeeze())**2), grad_calc, grad_approx
+
+
+
+if __name__ == "__main__":
+    netb  = NetworkBuilder()
+    netb.input(1) >> LstmLayer(1) >> netb.output
+    net = netb.build()
+
+    #weights = rnd.randn(net.get_param_size())
+    # and set them as the parameter buffer
+    #net.set_param_buffer(weights)
+
+    a, b, c = check_gradient(net)
+
+    #print(a)
+    #print(b.squeeze())
+    print(c)
+    print(b.squeeze()-c)
+
+
+
+
