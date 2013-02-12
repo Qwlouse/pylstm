3ff2ebe30471aa00d7ed037fa6df373921449f45
Klaus Greff <klaus@idsia.ch>
2013-02-12 14:33:35 +0100

    fixed test_network test

--- test/pylstm/test_network.py
+++ test/pylstm/test_network.py
@@ -15,7 +15,7 @@ class NetworkTests(unittest.TestCase):
         netb.input(5) >> LstmLayer(3) >> netb.output
         self.net = netb.build()
         self.net.set_param_buffer(np.random.randn(self.net.get_param_size()))
-        self.X = np.random.randn(1, 1, self.net.get_input_size())
+        self.X = np.random.randn(2, 7, self.net.get_input_size())
 
     def test_lstm_forward_pass_insensitive_to_internal_state(self):
         out1 = self.net.forward_pass(self.X).as_array().copy()
@@ -25,12 +25,13 @@ class NetworkTests(unittest.TestCase):
         self.assertTrue(np.allclose(out1, out2))
 
     def test_lstm_backward_pass_insensitive_to_internal_deltas(self):
-        out = self.net.forward_pass(self.X).as_array()
         self.net.clear_internal_state()
-        deltas1 = self.net.backward_pass(np.random.randn(self.net.get_output_size())).as_array().copy()
+        out1 = self.net.forward_pass(self.X).as_array().copy()
+        deltas1 = self.net.backward_pass(out1).as_array().copy()
         self.net.intern_manager.initialize_buffer(BufferView(np.random.randn(
             self.net.intern_manager.calculate_size())))
         self.net.delta_manager.initialize_buffer(BufferView(np.random.randn(
             self.net.delta_manager.calculate_size())))
-        deltas2 = self.net.backward_pass(out).as_array().copy()
+        out2 = self.net.forward_pass(self.X).as_array().copy()
+        deltas2 = self.net.backward_pass(out2).as_array().copy()
         self.assertTrue(np.allclose(deltas1, deltas2))
