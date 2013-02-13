f9ed5817744573be0c899dd9b83134f313382105
sohrob <sohrob@gmail.com>
2013-02-13 13:34:12 +0100

    possibly working utils grad checker

--- src/pylstm/utils2.py
+++ src/pylstm/utils2.py
@@ -14,32 +14,33 @@ rnd = np.random.RandomState(12345)
 
 
 def check_gradient(net):
-    timesteps = 3
-    n_batches = 1
+    timesteps = 5
+    n_batches = 5
 
-    #X = np.ones((timesteps, n_batches, net.get_input_size()))
-    X = rnd.randn(timesteps, n_batches, net.get_input_size())
-    #weights = np.ones(net.get_param_size())
-    weights = rnd.randn(net.get_param_size())
+    X = np.ones((timesteps, n_batches, net.get_input_size()))
+    #X = rnd.randn(timesteps, n_batches, net.get_input_size())
+    weights = np.ones(net.get_param_size())
+    #weights = rnd.randn(net.get_param_size())
     net.set_param_buffer(weights)
 
     out = net.forward_pass(X).as_array()
+    
     delta_calc = net.backward_pass(out).as_array()
     grad_calc = net.calc_gradient().as_array()
 
     def f(w):
         net.set_param_buffer(w)
         out = net.forward_pass(X).as_array()
-        return .5 * np.sum(out ** 2)/n_batches
+        return .5 * np.sum(out ** 2)#/n_batches
 
     grad_approx = approx_fprime(weights, f, 1e-7)
-    return np.sum((grad_approx - grad_calc.squeeze()) ** 2) / n_batches, grad_calc, grad_approx
+    return np.sum((grad_approx - grad_calc.squeeze()) ** 2), grad_calc, grad_approx
 
 
 
 if __name__ == "__main__":
     netb  = NetworkBuilder()
-    netb.input(1) >> LstmLayer(1) >> netb.output
+    netb.input(2) >> LstmLayer(3) >> netb.output
     net = netb.build()
 
     err, grad_calc, grad_approx = check_gradient(net)
