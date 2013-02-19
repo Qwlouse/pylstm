c3930f857c446c35c98449cdd31ab0c83e0220a9
sohrob <sohrob@gmail.com>
2013-02-19 17:50:07 +0100

    added broken resiliant prop --- needs matrix fix

--- src/pylstm/trainer.py
+++ src/pylstm/trainer.py
@@ -37,6 +37,50 @@ class SgdTrainer(object):
             grad_arr *= - self.learning_rate
             wrapper.add_into_b(grad, weights)
 
+class RPropTrainer(object):
+    def __init__(self, learning_rate=0.1, error_fkt=MeanSquaredError):
+        self.learning_rate = learning_rate
+        self.error_fkt = error_fkt()
+        self.initialized = False
+        
+
+    def train(self, net, X, T, epochs=100, callback=print_error_per_epoch):
+        weights = net.get_param_buffer()
+        for epoch in range(1, epochs + 1):
+            out = net.forward_pass(X).as_array()
+            error = self.error_fkt.forward_pass(out, T) / X.shape[1]
+            callback(epoch, error)
+            deltas = self.error_fkt.backward_pass(out, T)
+            net.backward_pass(deltas)
+            grad = net.calc_gradient()
+            grad_arr = grad.as_array()
+            print(weights.as_array().sum())
+
+            #calculate grad sign
+            grad_sign = (grad_arr > 0.0)
+            if not self.initialized:
+                self.last_grad_sign = grad_sign
+                self.stepsize = np.ones(net.get_param_size()) * .00000001
+                self.initialized = True
+                continue
+            increase = (grad_sign == self.last_grad_sign)
+            #print(self.stepsize)
+            self.stepsize = self.stepsize * (increase.flatten() * 1.01 + (increase.flatten() == False).flatten() * .99).flatten()
+            print("stepsize:", self.stepsize)
+            #OBexit
+            #print("self.stepsize")
+            #print(self.stepsize.flatten())
+            
+            grad_arr = self.stepsize * grad_sign.flatten() + -self.stepsize * (grad_sign == False).flatten()
+            print("grad arr:", grad_arr)
+            print("grad:", grad.as_array())
+            #print(((grad_sign==False)).flatten())
+            print("weights before:", weights.as_array().flatten())
+            wrapper.add_into_b(grad, weights)
+            print("weights after:", weights.as_array().flatten())
+            self.last_grad_sign = grad_sign.copy()
+            
+
 class CgTrainer(object):
     def __init__(self, learning_rate=0.1, error_fkt=MeanSquaredError):
         self.learning_rate = learning_rate
@@ -115,3 +159,6 @@ if __name__ == "__main__":
     X = rnd.randn(2, 5, 4)
     T = rnd.randn(2, 5, 3)
     trainer.train(net, X, T, epochs=10)
+    
+    trainer2 = CgTrainer()
+    trainer2.train(net, X, T)
