37a148fd2748dd95ad9005cb02daf2b09ce0b424
Klaus Greff <klaus@idsia.ch>
2013-02-25 13:16:32 +0100

    made fwd_layer compile

--- src/layers/fwd_layer.cpp
+++ src/layers/fwd_layer.cpp
@@ -5,16 +5,18 @@
 
 
 #include "fwd_layer.h"
-#include "matrix/matrix_operation_cpu.h"
+#include "matrix/matrix_operation.h"
+#include <vector>
 
-FwdWeights::FwdWeights(size_t n_inputs_, size_t n_cells_) :
+using std::vector;
+
+FwdWeights::FwdWeights(size_t n_inputs_, size_t n_cells_, Matrix& buffer) :
   n_inputs(n_inputs_), 
   n_cells(n_cells_), 
-
-  HX(n_cells, n_inputs); 
- 
-H_bias(n_cells, 1); 
+  HX(buffer.subslice(0, n_cells_, n_inputs_, 1)),
+  H_bias(buffer.subslice(n_cells_ * n_inputs_, n_cells_, 1, 1))
 {
+
 }
 
 size_t FwdWeights::buffer_size() {
@@ -22,19 +24,34 @@ size_t FwdWeights::buffer_size() {
 
 }
 
+void FwdWeights::allocate(Matrix buffer_view) {
+  vector<Matrix*> views;
+
+  views.push_back(&HX);
+  views.push_back(&H_bias);
+
+  lay_out(buffer_view, views);
+}
+
+
 FwdBuffers::FwdBuffers(size_t n_inputs_, size_t n_cells_, size_t n_batches_, size_t time_) :
   n_inputs(n_inputs_), n_cells(n_cells_),
   n_batches(n_batches_), time(time_),
-
-  //Views on all activations
-  Ha(n_cells, n_batches, time), Hb(n_cells, n_batches, time) //!< Input gate activation
-
+  Ha(n_cells, n_batches, time)
 {}
 
 size_t FwdBuffers::buffer_size() {
  //Views on all activations
-  return Ha.size + Hb.size; //!< Hidden unit activation
+  return Ha.size; //!< Hidden unit activation
     
+}
+
+void FwdBuffers::allocate(Matrix buffer_view) {
+  vector<Matrix*> views;
+
+  views.push_back(&Ha);
+
+  lay_out(buffer_view, views);
 }
 
 FwdDeltas::FwdDeltas(size_t n_inputs_, size_t n_cells_, size_t n_batches_, size_t time_) :
@@ -44,42 +61,38 @@ FwdDeltas::FwdDeltas(size_t n_inputs_, size_t n_cells_, size_t n_batches_, size_
 
   //Views on all activations
   Ha(n_cells, n_batches, time), Hb(n_cells, n_batches, time) //Input gate activation
- 
-  temp_hidden(n_cells, n_batches, time), temp_hidden2(n_cells, n_batches, time)
 {}
 
 size_t FwdDeltas::buffer_size() {
-  return Ha.size + Hb.size + //Hidden unit activation
-    temp_hidden.size + temp_hidden2.size; //temp vars
+  return Ha.size + Hb.size; //Hidden unit activation
 }
 
-void fwd_forward(FwdWeights &w, FwdBuffers &b, MatrixView3DCPU &x, MatrixView3DCPU &y) {
+void FwdDeltas::allocate(Matrix buffer_view) {
+  vector<Matrix*> views;
+
+  views.push_back(&Ha);
+  views.push_back(&Hb);
+
+  lay_out(buffer_view, views);
+}
+
+void fwd_forward(FwdWeights &w, FwdBuffers &b, Matrix &x, Matrix &y) {
   mult(w.HX, x.flatten(), b.Ha.flatten());
 
-  for (size_t t(0); t < b.time; ++t) {
-    add_into_b(w.H_bias, b.Ha.slice(t));
-    apply_sigmoid(b.Ha.slice(t), b.Hb.slice(t));
-      
-  }  
+  add_vector_into(w.H_bias, b.Ha);
+  apply_sigmoid(b.Ha, y);
 }
 
-void fwd_backward(FwdWeights &w, FwdBuffers &b, FwdDeltas &d, MatrixView3DCPU &y, MatrixView3DCPU &in_deltas, MatrixView3DCPU &out_deltas) {
+void fwd_backward(FwdWeights &w, FwdBuffers &b, FwdDeltas &d, Matrix &y, Matrix &in_deltas, Matrix &out_deltas) {
+  apply_sigmoid_deriv(y, d.Hb);
 
-  size_t end_time(b.time - 1);
+  dot(d.Hb, out_deltas, d.Ha);
 
-  //calculate t+1 values except for end_time+1 
-  for(int t(end_time); t >= 0; --t){
-    if (t<end_time) {
-      
-      mult(w.HH.T(), d.Hb.slice(t+1), d.Hb.slice(t));
-    }
-
-    // \f$\frac{dE}{da_H} = \frac{dE}{db_H} * f'(a_H)\f$
-    //sigmoid_deriv(d.Hb.slice(t), b.Hb.slice(t), d.temp_hidden, d.temp_hidden2, d.Ha.slice(t));    
-   
-  }
+  mult(w.HX.T(), d.Ha, in_deltas);
 }
 
+void fwd_grad(FwdWeights &w, FwdWeights &grad, FwdBuffers &b, FwdDeltas &d, Matrix &y, Matrix input_batches)  {
 
-
-
+  mult(d.Ha, input_batches.T(), grad.HX);
+  squash(d.Ha, grad.H_bias);
+}
--- src/layers/fwd_layer.h
+++ src/layers/fwd_layer.h
@@ -9,51 +9,56 @@
 #ifndef __FWD_LAYER_H__
 #define __FWD_LAYER_H__
 
-#include "matrix/matrix_cpu.h"
+#include "matrix/matrix.h"
 #include <iostream>
 
-struct FwdWeights {
-  size_t n_inputs, n_cells;
+class FwdWeights {
+public:
+	static size_t estimate_size(size_t n_inputs, size_t n_cells);
 
-  ///Variables defining sizes
-  MatrixView2DCPU HX;  //!< inputs X, H, S to input gate I 
 
-  MatrixView2DCPU H_bias;   //!< bias to input gate, forget gate, state Z, output gate
-  //MatrixCPU weights; 
+	size_t n_inputs, n_cells;
+	///Variables defining sizes
+	Matrix HX;  //!< inputs X, H, S to input gate I
+	Matrix H_bias;   //!< bias to input gate, forget gate, state Z, output gate
 
-  FwdWeights(size_t n_inputs, size_t n_cells);
+	FwdWeights(size_t n_inputs, size_t n_cells, Matrix& buffer);
 
-  size_t buffer_size();
+	size_t buffer_size();
+	void allocate(Matrix buffer_view);
 };
 
 struct FwdBuffers {
-  ///Variables defining sizes
-  size_t n_inputs, n_outputs, n_cells;
-  size_t n_batches, time;
+	///Variables defining sizes
+	size_t n_inputs, n_outputs, n_cells;
+	size_t n_batches, time;
 
-  //Views on all activations
-  MatrixView3DCPU Ha, Hb; //!< Hidden unit activation and output
+	//Views on all activations
+	Matrix Ha; //!< Hidden unit activation and output
 
-  FwdBuffers(size_t n_inputs_, size_t n_cells_, size_t n_batches, size_t time_);
-  
-  size_t buffer_size();
+	FwdBuffers(size_t n_inputs_, size_t n_cells_, size_t n_batches, size_t time_);
+
+	size_t buffer_size();
+	void allocate(Matrix buffer_view);
 };
 
 struct FwdDeltas {
-  ///Variables defining sizes
-  size_t n_inputs, n_outputs, n_cells;
-  size_t n_batches, time;
+	///Variables defining sizes
+	size_t n_inputs, n_outputs, n_cells;
+	size_t n_batches, time;
 
-  //Views on all activations
-  MatrixView3DCPU Ha, Hb; //Hidden unit activation and output
+	//Views on all activations
+	Matrix Ha, Hb; //Hidden unit activation and output
 
-  MatrixView3DCPU temp_hidden, temp_hidden2; //temp values, neccessary? 
+	//MatrixView3DCPU temp_hidden, temp_hidden2; //temp values, neccessary?
 
-  FwdDeltas(size_t n_inputs_, size_t n_cells_, size_t n_batches, size_t time_);
-  size_t buffer_size();
+	FwdDeltas(size_t n_inputs_, size_t n_cells_, size_t n_batches, size_t time_);
+	size_t buffer_size();
+	void allocate(Matrix buffer_view);
 };
 
-void fwd_forward(FwdWeights &w, FwdBuffers &b, MatrixView3DCPU &x, MatrixView3DCPU &y);
-void fwd_backward(FwdWeights &w, FwdBuffers &b, FwdDeltas &d, MatrixView3DCPU &y, MatrixView3DCPU &in_deltas, MatrixView3DCPU &out_deltas);
+void fwd_forward(FwdWeights &w, FwdBuffers &b, Matrix &x, Matrix &y);
+void fwd_backward(FwdWeights &w, FwdBuffers &b, FwdDeltas &d, Matrix &y, Matrix &in_deltas, Matrix &out_deltas);
+void fwd_grad(FwdWeights &w, FwdWeights &grad, FwdBuffers &b, FwdDeltas &d, Matrix &y, Matrix input_batches);
 
 #endif
