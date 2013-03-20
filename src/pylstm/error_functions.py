20952f1466e9ec65418336a84f09da0aade2501e
Klaus Greff <klaus@idsia.ch>
2013-03-20 13:16:17 +0100

    fixed cross entropy error

--- src/pylstm/error_functions.py
+++ src/pylstm/error_functions.py
@@ -2,6 +2,14 @@
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
 import numpy as np
+from pylstm.wrapper import Buffer
+
+
+def ensure_np_array(a):
+    if isinstance(a, Buffer):
+        return a.as_array()
+    else:
+        return np.array(a)
 
 
 class ErrorFunction(object):
@@ -14,17 +22,17 @@ class ErrorFunction(object):
 
 class MeanSquaredError(ErrorFunction):
     def evaluate(self, Y, T):
-        Y = Y.as_array()
+        Y = ensure_np_array(Y)
         return 0.5 * np.sum((Y - T) ** 2)
 
     def deriv(self, Y, T):
-        Y = Y.as_array()
+        Y = ensure_np_array(Y)
         return Y - T
 
 
 class CrossEntropyError(object):
     def __call__(self, Y, T):
-        Y = Y.as_array()
+        Y = ensure_np_array(Y)
         Y[Y < 1e-6] = 1e-6
         cee = T * np.log(Y)
         return - np.sum(cee)
@@ -33,6 +41,6 @@ class CrossEntropyError(object):
         return self(Y, T)
 
     def deriv(self, Y, T):
-        Y = Y.as_array()
-        return T / Y
+        Y = ensure_np_array(Y)
+        return - T / Y
 
