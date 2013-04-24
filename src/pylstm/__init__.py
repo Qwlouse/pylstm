21a8ee06d0360aebeaea88c229079b0ee7b9417e
Klaus Greff <klaus@idsia.ch>
2013-04-24 12:03:45 +0200

    added common imports to pylstm root package

--- src/pylstm/__init__.py
+++ src/pylstm/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 # coding=utf-8
 
+from netbuilder import NetworkBuilder
+from layers import RegularLayer, ReverseLayer, LstmLayer, RnnLayer
+from error_functions import MeanSquaredError, CrossEntropyError
