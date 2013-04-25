6b6a7a29794d9e9d8c62351b3b96a169110289e9
Klaus Greff <klaus@idsia.ch>
2013-04-25 14:56:15 +0200

    renamed pointer to C Matrix object to 'c_obj' from 'view'

--- src/pylstm/wrapper/py_matrix.pxd
+++ src/pylstm/wrapper/py_matrix.pxd
@@ -3,5 +3,5 @@ cimport numpy as np
 cimport c_matrix as cm
 
 cdef class Matrix:
-    cdef cm.Matrix view
+    cdef cm.Matrix c_obj
     cdef np.ndarray A
--- src/pylstm/wrapper/py_matrix.pyx
+++ src/pylstm/wrapper/py_matrix.pyx
@@ -14,39 +14,39 @@ cdef class Matrix:
         cdef np.ndarray[np.double_t, ndim=3, mode='c'] A
         if a is None:
             self.A = None
-            self.view = cm.Matrix()
+            self.c_obj = cm.Matrix()
         elif isinstance(a, int):
             self.A = None
-            self.view = cm.Matrix(features, batches, a)
+            self.c_obj = cm.Matrix(features, batches, a)
         else: # a is np array or iterable
             if len(a) == 0:
-                self.view = cm.Matrix()
+                self.c_obj = cm.Matrix()
                 self.A = None
                 return
             a = np.array(a)
             if len(a.shape) == 1:
                 a = a.reshape(-1, 1, 1)
             A = np.ascontiguousarray(a, dtype=np.float64)
-            self.view = cm.Matrix(&A[0,0,0], A.shape[2], A.shape[1], A.shape[0])
+            self.c_obj = cm.Matrix(&A[0,0,0], A.shape[2], A.shape[1], A.shape[0])
             self.A = A # make sure numpy array does not get GCed
 
     def __len__(self):
-        return self.view.size
+        return self.c_obj.size
 
     def __getitem__(self, item):
         if isinstance(item, int):
-            return self.view[item]
+            return self.c_obj[item]
         elif isinstance(item, slice):
             start = item.start or 0
             stop = item.stop or self.get_time_size()
 
             b = Matrix()
-            b.view = self.view.sub_matrix(start, self.view.n_rows, self.view.n_columns, stop-start)
+            b.c_obj = self.c_obj.sub_matrix(start, self.c_obj.n_rows, self.c_obj.n_columns, stop-start)
             b.A = self.A
             return b
             
     def __setitem__(self, int item, float value):
-        self.view[item] = value
+        self.c_obj[item] = value
 
     def feature_slice(self, start, stop=None):
         b = Matrix()
@@ -55,17 +55,17 @@ cdef class Matrix:
         if self.get_feature_size() == 0:
             assert start == 0
             assert stop is None or stop == 0
-            b.view = self.view
+            b.c_obj = self.c_obj
         else :
             assert start < self.get_feature_size()
             if stop is None:
-                b.view = self.view.row_slice(start)
+                b.c_obj = self.c_obj.row_slice(start)
             else:
                 assert start <= stop <= self.get_feature_size()
                 if start == stop:
-                    b.view = self.view.sub_matrix(0, 0, 0, 0)
+                    b.c_obj = self.c_obj.sub_matrix(0, 0, 0, 0)
                 else:
-                    b.view = self.view.row_slice(start, stop-1)
+                    b.c_obj = self.c_obj.row_slice(start, stop-1)
         return b
 
     def time_slice(self, start, stop=None):
@@ -75,14 +75,14 @@ cdef class Matrix:
         if self.get_time_size() == 0:
             assert start == 0
             assert stop is None or stop == 0
-            b.view = self.view
+            b.c_obj = self.c_obj
         else :
             assert start < self.get_time_size()
             if stop is None:
-                b.view = self.view.slice(start)
+                b.c_obj = self.c_obj.slice(start)
             else:
                 assert start <= stop <= self.get_time_size()
-                b.view = self.view.slice(start, stop-1)
+                b.c_obj = self.c_obj.slice(start, stop-1)
         return b
         
     # from here: https://gist.github.com/1249305
@@ -93,7 +93,7 @@ cdef class Matrix:
         shape[1] = <np.npy_intp> self.get_batch_size()
         shape[2] = <np.npy_intp> self.get_feature_size()
         # Create a 3D array
-        ndarray = np.PyArray_SimpleNewFromData(3, shape, np.NPY_FLOAT64, self.view.get_data())
+        ndarray = np.PyArray_SimpleNewFromData(3, shape, np.NPY_FLOAT64, self.c_obj.get_data())
         # Assign our object to the 'base' of the ndarray object
         ndarray.base = <PyObject*> self
         # Increment the reference count, as the above assignment was done in
@@ -102,13 +102,13 @@ cdef class Matrix:
         return ndarray
 
     def get_feature_size(self):
-        return self.view.n_rows
+        return self.c_obj.n_rows
 
     def get_batch_size(self):
-        return self.view.n_columns
+        return self.c_obj.n_columns
 
     def get_time_size(self):
-        return self.view.n_slices
+        return self.c_obj.n_slices
 
     def shape(self):
         return self.get_time_size(), self.get_batch_size(), self.get_feature_size()
@@ -132,55 +132,55 @@ cdef class Matrix:
         assert time_size * batch_size * feature_size == len(self)
         b = Matrix()
         b.A = self.A
-        b.view = cm.Matrix(self.view)
-        b.view.n_rows = feature_size
-        b.view.n_columns = batch_size
-        b.view.n_slices = time_size 
+        b.c_obj = cm.Matrix(self.c_obj)
+        b.c_obj.n_rows = feature_size
+        b.c_obj.n_columns = batch_size
+        b.c_obj.n_slices = time_size
         return b
 
     def print_me(self):
-        self.view.print_me()
+        self.c_obj.print_me()
 
     def __repr__(self):
         t, b, f = self.shape()
         return "<Matrix (%d, %d, %d) at %#x>"%(t, b, f, id(self))
 
     def set_all_elements_to(self, value):
-        self.view.set_all_elements_to(value)
+        self.c_obj.set_all_elements_to(value)
 
 
 def dot(Matrix a not None, Matrix b not None, Matrix out not None):
-    cm.dot(a.view, b.view, out.view)
+    cm.dot(a.c_obj, b.c_obj, out.c_obj)
 
 #def add(MatrixCPU a not None, MatrixCPU b not None, MatrixCPU out not None):
 #    cm.add(a.view, b.view, out.view)
 
 def add_into_b(Matrix a not None, Matrix b not None):
-    cm.add_into_b(a.view, b.view)
+    cm.add_into_b(a.c_obj, b.c_obj)
 
 def add_scalar(Matrix a not None, double b):
-    cm.add_scalar(a.view, b)
+    cm.add_scalar(a.c_obj, b)
 
 def mult(Matrix a not None, Matrix b not None, Matrix out not None):
-    cm.mult(a.view, b.view, out.view)
+    cm.mult(a.c_obj, b.c_obj, out.c_obj)
 
 def mult_add(Matrix a not None, Matrix b not None, Matrix out not None):
-    cm.mult_add(a.view, b.view, out.view)
+    cm.mult_add(a.c_obj, b.c_obj, out.c_obj)
 
 def dot(Matrix a not None, Matrix b not None, Matrix out not None):
-    cm.dot(a.view, b.view, out.view)
+    cm.dot(a.c_obj, b.c_obj, out.c_obj)
 
 def dot_add(Matrix a not None, Matrix b not None, Matrix out not None):
-    cm.dot_add(a.view, b.view, out.view)
+    cm.dot_add(a.c_obj, b.c_obj, out.c_obj)
 
 def apply_sigmoid(Matrix a not None, Matrix out not None):
-    cm.add_into_b(a.view, out.view)
+    cm.add_into_b(a.c_obj, out.c_obj)
 
 def apply_tanh(Matrix a not None, Matrix out not None):
-    cm.add_into_b(a.view, out.view)
+    cm.add_into_b(a.c_obj, out.c_obj)
 
 def apply_tanhx2(Matrix a not None, Matrix out not None):
-    cm.add_into_b(a.view, out.view)
+    cm.add_into_b(a.c_obj, out.c_obj)
 
 def equals(Matrix a not None, Matrix out not None):
-    cm.add_into_b(a.view, out.view)
+    cm.add_into_b(a.c_obj, out.c_obj)
