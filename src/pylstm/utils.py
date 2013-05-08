3250525b0ae3267773d708b8094967db3e771f51
Klaus Greff <klaus@idsia.ch>
2013-05-08 14:14:42 +0200

    added check_gn_pass to utils and made it run for multi-batch

--- src/pylstm/utils.py
+++ src/pylstm/utils.py
@@ -6,7 +6,8 @@ from scipy.optimize import approx_fprime
 import numpy as np
 
 
-def check_gradient(net, X=None, T=None, n_timesteps=3, n_batches=5, rnd=np.random.RandomState()):
+def check_gradient(net, X=None, T=None, n_timesteps=3, n_batches=5,
+                   rnd=np.random.RandomState()):
     if X is None:
         X = rnd.randn(n_timesteps, n_batches, net.get_input_size())
     if T is None:
@@ -31,7 +32,8 @@ def check_gradient(net, X=None, T=None, n_timesteps=3, n_batches=5, rnd=np.rando
     return np.sum((grad_approx - grad_calc) ** 2) / n_batches, grad_calc, grad_approx
 
 
-def check_deltas(net, X=None, T=None, n_timesteps=3, n_batches=5, rnd=np.random.RandomState()):
+def check_deltas(net, X=None, T=None, n_timesteps=3, n_batches=5,
+                 rnd=np.random.RandomState()):
     if X is None:
         X = rnd.randn(n_timesteps, n_batches, net.get_input_size())
     if T is None:
@@ -52,9 +54,11 @@ def check_deltas(net, X=None, T=None, n_timesteps=3, n_batches=5, rnd=np.random.
     return np.sum((delta_approx - delta_calc) ** 2) / n_batches, delta_calc, delta_approx
 
 
-def check_rpass(net, X=None, r=1e-7, n_timesteps=3, n_batches=5, rnd=np.random.RandomState()):
+def check_rpass(net, X=None, r=1e-7, n_timesteps=3, n_batches=5,
+                rnd=np.random.RandomState()):
     if X is None:
         X = rnd.randn(n_timesteps, n_batches, net.get_input_size())
+    nr_timesteps, nr_batches, _ = X.shape
 
     weights = net.param_buffer.copy()
     errs = np.zeros_like(weights)
@@ -73,3 +77,42 @@ def check_rpass(net, X=None, r=1e-7, n_timesteps=3, n_batches=5, rnd=np.random.R
 
     return np.sum(errs**2), errs
 
+
+def estimate_jacobian(net, X=None, nr_timesteps=4, nr_batches=5,
+                      rnd=np.random.RandomState()):
+    out_size = net.get_output_size()
+    in_size = net.get_input_size()
+    if X is None:
+        X = rnd.randn(nr_timesteps, nr_batches, in_size)
+    nr_timesteps, nr_batches, _ = X.shape
+
+    out = net.forward_pass(X).copy()
+    J = np.zeros((out_size * nr_timesteps * nr_batches, net.get_param_size()))
+
+    for i in range(out_size * nr_timesteps * nr_batches):
+        out_masked = np.zeros_like(out).flatten()
+        out_masked[i] = 1.0
+        out_masked = out_masked.reshape(*out.shape)
+
+        deltas = net.pure_backpass(out_masked)
+        J[i, :] = net.calc_gradient().flatten()
+    return J
+
+
+def check_gn_pass(net, X=None, v=None, r=1e-7, nr_timesteps=3, nr_batches=5,
+                  rnd=np.random.RandomState()):
+    out_size = net.get_output_size()
+    in_size = net.get_input_size()
+    if X is None:
+        X = rnd.randn(nr_timesteps, nr_batches, in_size)
+    nr_timesteps, nr_batches, _ = X.shape
+
+    if v is None:
+        v = rnd.randn(net.get_param_size())
+
+    J = estimate_jacobian(net, X)
+    G = J.T.dot(J)
+
+    calc = net.hessian_pass(X, v).flatten()
+    estimated = G.dot(v).flatten()
+    return np.sum((calc - estimated) ** 2), calc, estimated
--- src/test_gauss_newton.py
+++ src/test_gauss_newton.py
@@ -4,9 +4,10 @@
 from __future__ import division, print_function, unicode_literals
 from pylstm import NetworkBuilder, RegularLayer, RnnLayer, LstmLayer, ReverseLayer
 import numpy as np
+from pylstm.utils import check_gn_pass
 
 
-def estimate_jacobian(net, X=None, nr_timesteps=4, nr_batches=1, rnd=np.random.RandomState()):
+def estimate_jacobian(net, X=None, nr_timesteps=4, nr_batches=5, rnd=np.random.RandomState()):
     out_size = net.get_output_size()
     in_size = net.get_input_size()
     if X is None:
@@ -14,9 +15,9 @@ def estimate_jacobian(net, X=None, nr_timesteps=4, nr_batches=1, rnd=np.random.R
     nr_timesteps, nr_batches, _ = X.shape
 
     out = net.forward_pass(X).copy()
-    J = np.zeros((out_size * nr_timesteps, net.get_param_size()))
+    J = np.zeros((out_size * nr_timesteps * nr_batches, net.get_param_size()))
 
-    for i in range(out_size * nr_timesteps):
+    for i in range(out_size * nr_timesteps * nr_batches):
         out_masked = np.zeros_like(out).flatten()
         out_masked[i] = 1.0
         out_masked = out_masked.reshape(*out.shape)
@@ -26,15 +27,11 @@ def estimate_jacobian(net, X=None, nr_timesteps=4, nr_batches=1, rnd=np.random.R
     return J
 
 
-
-
-
-
 rnd = np.random.RandomState(2893749283)
 
 in_size = 2
 out_size = 2
-nr_batches = 1
+nr_batches = 32
 nr_timesteps = 4
 
 netb = NetworkBuilder()
@@ -44,49 +41,29 @@ net = netb.build()
 
 
 net.param_buffer = rnd.randn(net.get_param_size())
-X1 = rnd.randn(nr_timesteps, nr_batches, in_size)
-X2 = rnd.randn(nr_timesteps, nr_batches, in_size)
+X = rnd.randn(nr_timesteps, nr_batches, in_size)
 
+J = estimate_jacobian(net, X, rnd=rnd)
 
-J1 = estimate_jacobian(net, X1, rnd=rnd)
-J2 = estimate_jacobian(net, X2, rnd=rnd)
+G = J.T.dot(J)
 
-G1 = J1.T.dot(J1)
-G2 = J2.T.dot(J2)
-print(G1)
+print(G)
 v = np.arange(net.get_param_size())
 
-o1 = net.hessian_pass(X1, v).flatten()
-p1 = G1.dot(v).flatten()
+o = net.hessian_pass(X, v).flatten()
+p = G.dot(v).flatten()
 
 print('hessian_pass')
-print(o1)
+print(o)
 print('jacobi approx')
-print(p1)
+print(p)
 print('difference')
-print(o1 - p1)
+print(o - p)
 
-print('Sum of squares error', np.sum(np.abs(o1-p1)))
+print('Sum of squares error', np.sum((o-p)**2))
 print('\n\n')
 
-o2 = net.hessian_pass(X2, v).flatten()
-p2 = G2.dot(v).flatten()
-
-print('hessian_pass')
-print(o2)
-print('jacobi approx')
-print(p2)
-print('difference')
-print(o2 - p2)
-
-print('Sum of squares error', np.sum(np.abs(o2-p2)))
-print('\n\n')
-
-print('added Hessians')
-
-
-
-
+print(check_gn_pass(net, X, v))
 
 
 
