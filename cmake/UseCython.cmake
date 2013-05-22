c2d51d4b3d82cc67386b84e0b3e6344f3d409670
Klaus Greff <klaus@idsia.ch>
2013-05-22 11:54:59 +0200

    converted replicate python source tree to bash script

--- cmake/UseCython.cmake
+++ cmake/UseCython.cmake
@@ -62,6 +62,45 @@
 # limitations under the License.
 #=============================================================================
 
+
+IF (${CMAKE_VERSION} VERSION_LESS 2.8.3)
+  MACRO(CMAKE_PARSE_ARGUMENTS prefix arg_names option_names)
+    SET(DEFAULT_ARGS)
+    FOREACH(arg_name ${arg_names})
+      SET(${prefix}_${arg_name})
+    ENDFOREACH(arg_name)
+    FOREACH(option ${option_names})
+      SET(${prefix}_${option} FALSE)
+    ENDFOREACH(option)
+
+    SET(current_arg_name DEFAULT_ARGS)
+    SET(current_arg_list)
+    FOREACH(arg ${ARGN})
+      SET(larg_names ${arg_names})
+      LIST(FIND larg_names "${arg}" is_arg_name)
+      IF (is_arg_name GREATER -1)
+        SET(${prefix}_${current_arg_name} ${current_arg_list})
+        SET(current_arg_name ${arg})
+        SET(current_arg_list)
+      ELSE (is_arg_name GREATER -1)
+        SET(loption_names ${option_names})
+        LIST(FIND loption_names "${arg}" is_option)
+        IF (is_option GREATER -1)
+           SET(${prefix}_${arg} TRUE)
+        ELSE (is_option GREATER -1)
+           SET(current_arg_list ${current_arg_list} ${arg})
+        ENDIF (is_option GREATER -1)
+      ENDIF (is_arg_name GREATER -1)
+    ENDFOREACH(arg)
+    SET(${prefix}_${current_arg_name} ${current_arg_list})
+  ENDMACRO(PARSE_ARGUMENTS)
+
+ELSE (${CMAKE_VERSION} VERSION_LESS 2.8.3)
+  include( CMakeParseArguments )
+
+ENDIF (${CMAKE_VERSION} VERSION_LESS 2.8.3)
+
+
 # Configuration options.
 set( CYTHON_ANNOTATE OFF
   CACHE BOOL "Create an annotated .html file when compiling *.pyx." )
@@ -245,7 +284,6 @@ function( cython_add_module _name )
   target_link_libraries( ${_name} ${PYTHON_LIBRARIES} )
 endfunction()
 
-include( CMakeParseArguments )
 # cython_add_standalone_executable( _name [MAIN_MODULE src3.py] src1 src2 ... srcN )
 # Creates a standalone executable the given sources.
 function( cython_add_standalone_executable _name )
