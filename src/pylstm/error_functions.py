fixed some underflow errors in CTC

src/pylstm/error_functions.py
@@ -82,7 +82,7 @@ def ctc_calculate_alphas(Y_log, T):
             alpha[t, s] += Y_log[t, label]
             previous_label = label
 
-    return np.exp(alpha)
+    return alpha
 
 
 def ctc_calculate_betas(Y_log, T):
@@ -109,7 +109,7 @@ def ctc_calculate_betas(Y_log, T):
                 label = T[(s + 2) // 2]
                 if label != previous_label:
                     beta[t - 1, s] = np.logaddexp(beta[t - 1, s], beta[t, s + 2] + Y_log[t, label])
-    return np.exp(beta)
+    return beta
 
 def CTC(Y, T, M=None):
     import warnings
@@ -128,6 +128,7 @@ def CTC(Y, T, M=None):
         # calculate forward variables alpha
         ## set up the dynamic programming matrix
         deltas = np.zeros((N, batch_size, label_count))
+        deltas[:] = neg_inf
         errors = []
         for b, (y, t, m) in enumerate(Online(Y_log, T, M)):
             t = t[0]
@@ -144,16 +145,16 @@ def CTC(Y, T, M=None):
             alpha = ctc_calculate_alphas(y, t)
             beta = ctc_calculate_betas(y, t)
 
-            ppix = alpha * beta
-            pzx = ppix.sum(1)
+            ppix = alpha + beta
+            pzx = np.logaddexp.accumulate(ppix, axis=1)[:, -1]
 
-            deltas[:, b, 0] = ppix[:, ::2].sum(1)
+            deltas[:, b, 0] = np.logaddexp.accumulate(ppix[:, ::2], axis=1)[:, -1]
             for s in range(1, 2 * S + 1, 2):
-                deltas[:, b, t[s // 2]] += ppix[:, s]
+                deltas[:, b, t[s // 2]] = np.logaddexp(deltas[:, b, t[s // 2]], ppix[:, s])
             for l in range(label_count):
-                deltas[:, b, l] /= - np.exp(y[:, l]) * pzx
+                deltas[:, b, l] -= y[:, l] + pzx
 
-            errors.append(-(np.log(ppix.sum(1))).mean())
+            errors.append(-pzx.mean())
 
-        return np.mean(errors), deltas / batch_size
+        return np.mean(errors), -np.exp(deltas) / batch_size
 

test/pylstm/test_error_funcs.py
@@ -67,7 +67,7 @@ class CTCTest(unittest.TestCase):
              [0, .56, .192, 0],
              [0, .07, .284, .1048],
              [0, 0, .021, .2135]]).T
-        self.assertTrue(np.allclose(a, a_expected))
+        self.assertTrue(np.allclose(np.exp(a), a_expected))
 
     def test_beta_values(self):
         b = ctc_calculate_betas(np.log(self.Y), self.T)
@@ -77,7 +77,7 @@ class CTCTest(unittest.TestCase):
              [0, .42, .2, 0],
              [0, .57, .9, 1],
              [0, 0, .7, 1]]).T
-        self.assertTrue(np.allclose(b, b_expected))
+        self.assertTrue(np.allclose(np.exp(b), b_expected))
 
     def test_alpha_values_duplicate_label(self):
         T = np.array([1, 1])
@@ -88,7 +88,7 @@ class CTCTest(unittest.TestCase):
              [0, .56, .192, 0],
              [0, 0, .168, .036],
              [0, 0, 0, .1176]]).T
-        self.assertTrue(np.allclose(a, a_expected))
+        self.assertTrue(np.allclose(np.exp(a), a_expected))
 
     def test_beta_values_duplicate_label(self):
         T = np.array([1, 1])
@@ -99,7 +99,7 @@ class CTCTest(unittest.TestCase):
              [0, .27, .1, 0],
              [0, .45, .8, 1],
              [0, 0, .7, 1]]).T
-        self.assertTrue(np.allclose(b, b_expected))
+        self.assertTrue(np.allclose(np.exp(b), b_expected))
 
     @unittest.skip
     def test_forward_values_multibatch1(self):
@@ -129,7 +129,7 @@ class CTCTest(unittest.TestCase):
     def test_pxz_equal_for_all_t(self):
         a = ctc_calculate_alphas(np.log(self.Y), self.T)
         b = ctc_calculate_betas(np.log(self.Y), self.T)
-        pxz = (a * b).T.sum(0)
+        pxz = np.exp(a + b).T.sum(0)
         self.assertTrue(np.allclose(pxz, pxz.mean()))
 
     @unittest.skip