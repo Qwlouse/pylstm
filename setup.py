b22afd6d9b9ab912b48a80f501498e05bd582cd5
Klaus Greff <klaus@idsia.ch>
2013-08-16 17:18:21 +0200

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from distutils.core import setup
 
 setup(
     name="PyLSTM",
-    version="0.1",
-    packages=['pylstm', 'pylstm.wrapper'],
+    version="0.2",
+    packages=['pylstm', 'pylstm.wrapper', 'pylstm.layers'],
     package_data={'pylstm.wrapper': ['*.so']},
     requires=['numpy', 'scipy'],
 )
