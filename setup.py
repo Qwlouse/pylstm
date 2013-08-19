4660026f036ecde3c43c07427233e4fd4144a927
Klaus Greff <klaus@idsia.ch>
2013-08-19 18:56:17 +0200

    added the additional packages to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from distutils.core import setup
 setup(
     name="PyLSTM",
     version="0.2",
-    packages=['pylstm', 'pylstm.wrapper', 'pylstm.layers'],
+    packages=['pylstm', 'pylstm.wrapper', 'pylstm.structure', 'pylstm.datasets',
+              'pylstm.regularization', 'pylstm.training'],
     package_data={'pylstm.wrapper': ['*.so']},
     requires=['numpy', 'scipy'],
 )
