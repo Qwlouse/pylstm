20137818dc9694bdb0f384877b7b7d82865a3903
Klaus Greff <klaus@idsia.ch>
2013-08-27 18:16:52 +0200

    bumped up CopyFromNetwork

--- src/pylstm/regularization/initializer.py
+++ src/pylstm/regularization/initializer.py
@@ -4,6 +4,17 @@ from __future__ import division, print_function, unicode_literals
 import numpy as np
 
 
+class InitializationFailedError(Exception):
+    pass
+
+
+def evaluate_initializer(initializer, layer_name, view_name, shape, seed):
+    if callable(initializer):
+        return initializer(layer_name, view_name, shape, seed)
+    else:
+        return np.array(initializer)
+
+
 class Gaussian(object):
     def __init__(self, mean=0.0, std=1.0):
         self.mean = mean
@@ -28,30 +39,42 @@ class Uniform(object):
 
 
 class CopyFromNetwork(object):
-    def __init__(self, net, default=0):
+    def __init__(self, net, layer_name=None, view_name=None,
+                 on_missing_view=None, on_shape_mismatch=None):
         self.net = net
-        self.default = default
+        self.layer_name = layer_name
+        self.view_name = view_name
+        self.on_missing_view = on_missing_view
+        self.on_shape_mismatch = on_shape_mismatch
 
     def __call__(self, layer_name, view_name, shape, seed=None):
+        layer_name = layer_name if self.layer_name is None else self.layer_name
+        view_name = view_name if self.view_name is None else self.view_name
+
         if layer_name in self.net.layers:
             layer_view = self.net.get_param_view_for(layer_name)
             if view_name in layer_view:
                 view = layer_view[view_name]
                 if view.shape == shape:
                     return view
+                elif self.on_shape_mismatch is not None:
+                    return evaluate_initializer(self.on_shape_mismatch,
+                                                layer_name, view_name, shape,
+                                                seed)
                 else:
-                    print('CopyFromNetwork: Shape mismatch %s != %s in view %s '
-                          'of %s.' % (view.shape, shape, view_name, layer_name))
+                    raise InitializationFailedError('Shape mismatch %s != %s '
+                                                    'in view %s of %s.' %
+                                                    (view.shape, shape,
+                                                     view_name, layer_name))
+            elif self.on_missing_view is not None:
+                return evaluate_initializer(self.on_missing_view,
+                                            layer_name, view_name, shape,
+                                            seed)
             else:
-                print('CopyFromNetwork: View %s not found in layer %s.' %
-                      (view_name, layer_name))
+                raise InitializationFailedError('View %s not found in layer %s.'
+                                                % (view_name, layer_name))
         else:
-            print('CopyFromNetwork: Layer %s not found.' % layer_name)
-
-        if callable(self.default):
-            return self.default(layer_name, view_name, shape, seed)
-        else:
-            return self.default
+            raise InitializationFailedError('Layer %s not found.' % layer_name)
 
 
 class InSparse(object):
