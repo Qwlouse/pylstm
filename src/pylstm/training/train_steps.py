185c9acdfb82913054f4856f8794bffb07dcc87d
Rupesh Kumar Srivastava <rupesh@idsia.ch>
2013-09-05 17:02:11 +0200

    Reimplemented RPropStep

--- src/pylstm/training/train_steps.py
+++ src/pylstm/training/train_steps.py
@@ -97,14 +97,32 @@ class NesterovStep(object):
 
 
 class RPropStep(object):
-    def __init__(self, learning_rate=0.1):
-        self.learning_rate = learning_rate
-        self.net = None
+    """
+    References:
+    Improving the Rprop Learning Algorithm. Igel and Husken (2000).
+    Rprop - Description and Implementation Details. Reidmiller (1994).
+
+    Rprop default is Rprop+ which includes backtracking (even when error drops and gradient changes sign)
+    Rprop- can be obtained by setting backtracking = False
+    iRprop+ can be obtained by setting backtracking = True but backtrack_on_error_drop = False
+    """
+    def __init__(self, eta_minus=0.5, eta_plus=1.2, delta_0=0.1, delta_min=1e-6, delta_max=50,
+                 backtracking=True, backtrack_on_error_drop=True):
+        self.eta_plus = eta_plus
+        self.eta_minus = eta_minus
+        self.delta = delta_0
+        self.delta_min = delta_min
+        self.delta_max = delta_max
         self.initialized = False
+        self.backtracking = backtracking
+        self.backtrack_on_error_drop = backtrack_on_error_drop
 
     def start(self, net):
         self.net = net
-        self.initialized = False
+        self.last_grad_sign = 0
+        self.last_update = 0
+        self.last_error = np.Inf
+        self.initialized = True
 
     def run(self, x, t, m):
         self.net.forward_pass(x)
@@ -112,22 +130,32 @@ class RPropStep(object):
         self.net.backward_pass(t, m)
         grad = self.net.calc_gradient()
 
-        #calculate grad sign
-        grad_sign = (grad > 0.0)
+        grad_sign = np.sign(grad)
+        sign_flip = grad_sign * self.last_grad_sign
 
-        if not self.initialized:
-            self.last_grad_sign = grad_sign
-            self.stepsize = np.ones_like(grad_sign)
-            self.initialized = True
-            return error
+        # Calculate the delta
+        self.delta = (self.eta_plus * self.delta) * (sign_flip > 0) + \
+                     (self.eta_minus * self.delta) * (sign_flip < 0) + \
+                      self.delta * (sign_flip == 0)
+        self.delta = np.clip(self.delta, self.delta_min, self.delta_max)
 
-        increase = (grad_sign == self.last_grad_sign)
-        self.stepsize = (
-            self.stepsize * (increase * 1.01 + (increase == False) * .99))
+        # Calculate the update
+        if self.backtracking:
+            if self.backtrack_on_error_drop:
+                update = (-np.sign(grad) * self.delta * (sign_flip >= 0)) + \
+                         (-self.last_update * (sign_flip < 0))
+            else:
+                update = (-np.sign(grad) * self.delta * (sign_flip >= 0)) + \
+                         (-self.last_update * (sign_flip < 0) * (error > self.last_error))
+        else:
+            update = -np.sign(grad) * self.delta
 
-        grad[:] = self.stepsize * grad_sign + -self.stepsize * (
-            grad_sign == False)
+        # Update
+        self.net.param_buffer += update
 
-        self.net.param_buffer -= grad * self.learning_rate
-
-        self.last_grad_sign = grad_sign.copy()
+        if self.backtracking:
+            self.last_grad_sign = grad_sign.copy() * (sign_flip >= 0)
+        else:
+            self.last_grad_sign = grad_sign.copy()
+        self.last_update = update.copy()
+        self.last_error = error
