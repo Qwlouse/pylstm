0a206723622cacd8f4c4bc27166d76c13f3059b6
Klaus Greff <klaus@idsia.ch>
2013-09-20 14:57:27 +0200

    changed callbacks to monitor

--- src/pylstm/training/trainer.py
+++ src/pylstm/training/trainer.py
@@ -14,15 +14,28 @@ class Trainer(object):
         self.stepper = core if core else SgdStep(**kwargs)
         self.validation_stepper = ForwardStep()
         self.stopping_criteria = []
-        self.callbacks = [print_error_per_epoch]
         self.training_errors = []
         self.validation_errors = []
+        self.monitor = dict()
         self.epochs_seen = 0
 
-    def emit_callbacks(self):
-        for cb in self.callbacks:
-            cb(self.epochs_seen, self.net,
-               self.training_errors, self.validation_errors)
+    def emit_monitoring_batchwise(self, update_nr):
+        for mon in self.monitor.values():
+            per_epoch, interval = get_monitor_params(mon)
+            if per_epoch:
+                continue
+            if update_nr % interval == 0:
+                mon(self.epochs_seen, self.net, self.stepper,
+                    self.training_errors, self.validation_errors)
+
+    def emit_monitoring_epochwise(self):
+        for mon in self.monitor.values():
+            per_epoch, interval = get_monitor_params(mon)
+            if not per_epoch:
+                continue
+            if self.epochs_seen % interval == 0:
+                mon(self.epochs_seen, self.net, self.stepper,
+                    self.training_errors, self.validation_errors)
 
     def should_stop(self):
         for sc in self.stopping_criteria:
@@ -48,8 +61,10 @@ class Trainer(object):
             train_errors = []
             print("\nTraining ...")
             start = time.time()
-            for x, t, m in training_data_getter():
+            for i, (x, t, m) in enumerate(training_data_getter()):
                 train_errors.append(self.stepper.run(x, t, m))
+                self.emit_monitoring_batchwise(i + 1)
+
             print("Wall Time taken: ", time.time() - start)
 
             train_error = np.mean(train_errors)
@@ -67,8 +82,12 @@ class Trainer(object):
                 self.validation_errors.append(valid_error)
 
             self.epochs_seen += 1
-
-            self.emit_callbacks()
-
+            self.emit_monitoring_epochwise()
             if self.should_stop():
                 return train_error
+
+
+def get_monitor_params(monitor):
+    per_epoch = monitor.per_epoch if hasattr(monitor, 'per_epoch') else True
+    interval = monitor.interval if hasattr(monitor, 'interval') else 1
+    return per_epoch, interval
