05e9b15b5a4d77a27fb4a2c607da8ed96ea4f0f7
Klaus Greff <klaus@idsia.ch>
2013-09-26 15:17:14 +0200

    added a binarize array function

--- src/pylstm/datasets/preprocessing.py
+++ src/pylstm/datasets/preprocessing.py
@@ -17,6 +17,22 @@ def binarize_sequence(seq, alphabet=None):
     return result
 
 
+def binarize_array(A, alphabet=None):
+    if A.shape == 3:
+        assert A.shape[2] == 1
+        A = A[:, :, 0]
+    if alphabet is None:
+        alphabet = list(np.unique(A))
+    else:
+        alphabet = list(alphabet)
+    result = np.zeros((A.shape[0], A.shape[1], len(alphabet)))
+    for seq_nr in range(A.shape[1]):
+        for t in range(A.shape[0]):
+            index = alphabet.index(A[t, seq_nr])
+            result[t, seq_nr, index] = 1
+    return result
+
+
 def get_mean_masked(X, M):
     """
     Get the mean values for every feature in the batch of sequences X by
