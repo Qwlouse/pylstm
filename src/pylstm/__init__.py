0ce4a1cf9ee409b69ffa527847a6cf6fda5585f8
Rupesh Kumar Srivastava <rupspace@gmail.com>
2013-10-05 11:04:47 +0200

    Modified imports to import everything.

--- src/pylstm/__init__.py
+++ src/pylstm/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # coding=utf-8
 
-from .datasets import generate_memo_task, generate_5bit_memory_task
-from .datasets import generate_5bit_memory_task
+from .datasets import *
 from .error_functions import *
 from .regularization import *
 from .structure import *
--- src/pylstm/datasets/__init__.py
+++ src/pylstm/datasets/__init__.py
@@ -3,3 +3,8 @@
 
 from __future__ import division, print_function, unicode_literals
 from toy_problems import *
+from hierarchical_seq_pred import *
+from MFCC import *
+from loader import *
+from preprocessing import *
+from timit_dataset import *
