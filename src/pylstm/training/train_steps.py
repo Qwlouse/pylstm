9ef45afb0231c08050b30700b34e53383d535ddd
Klaus Greff <klaus@idsia.ch>
2014-02-02 05:24:08 +0100

    converted CgLiteTrainer to CgStep

--- src/pylstm/training/train_steps.py
+++ src/pylstm/training/train_steps.py
@@ -3,7 +3,9 @@
 
 from __future__ import division, print_function, unicode_literals
 import numpy as np
+from pylstm.training.data_iterators import Minibatches
 from pylstm.targets import Targets
+from pylstm.training.cg_trainer import conjgrad3
 
 
 class DiagnosticStep(object):
@@ -193,3 +195,85 @@ class RmsPropStep(object):
         #self.net.param_buffer += update.flatten()
 
         return error
+
+
+class CgStep(object):
+    def __init__(self, minibatch_size=32, mu=1./30, maxiter=300):
+        self.minibatch_size = minibatch_size
+        self.mu = mu
+        self.lambda_ = 0.1
+        self.maxiter = maxiter
+        self.net = None
+
+    def start(self, net):
+        self.net = net
+
+    def run(self, X, T, M):
+        mb = Minibatches(X, T, M, self.minibatch_size, verbose=False)()
+        x, t, m = mb.next()
+        ## calculate the gradient
+        grad = np.zeros_like(self.net.param_buffer.flatten())
+        error = []
+        for x, t, m in Minibatches(X, T, M, self.minibatch_size, verbose=False,
+                                   shuffle=False)():
+            self.net.forward_pass(x)
+            self.net.backward_pass(t, m)
+            error.append(self.net.calculate_error(t, m))
+            grad += self.net.calc_gradient().flatten()
+
+        grad /= self.minibatch_size
+        #grad *= -1
+        error = np.mean(error)
+        print(error)
+
+        ## initialize v
+        #v = np.zeros(net.get_param_size())
+        v = .01 * np.random.randn(self.net.get_param_size())
+
+        ## define hessian pass
+        def fhess_p(v):
+            return self.net.hessian_pass(x, v, self.mu, self.lambda_).copy().\
+                       flatten() / self.minibatch_size
+
+        ## run CG
+        all_v = conjgrad3(grad, v.copy(), fhess_p, maxiter=self.maxiter)
+
+        ## backtrack #1
+        lowError = float('Inf')
+        lowIdx = 0
+        weights = self.net.param_buffer.copy()
+        for i, testW in reversed(list(enumerate(all_v))):
+            self.net.param_buffer = weights - testW
+            self.net.forward_pass(x)
+            tmpError = self.net.calculate_error(t, m)
+            if tmpError < lowError:
+                lowError = tmpError
+                lowIdx = i
+        bestDW = all_v[lowIdx]
+
+        ## backtrack #2
+        finalDW = bestDW
+        for j in np.arange(0, 1.0, 0.1):
+            tmpDW = j * bestDW
+            self.net.param_buffer = weights - tmpDW
+            self.net.forward_pass(x)
+            tmpError = self.net.calculate_error(t, m)
+            if tmpError < lowError:
+                finalDW = tmpDW
+                lowError = tmpError
+
+        ## Levenberg-Marquardt heuristic
+        boost = 3.0 / 2.0
+        self.net.param_buffer = weights
+        denom = 0.5 * (np.dot(finalDW, fhess_p(finalDW))) + np.dot(
+            np.squeeze(grad), finalDW) + error
+        rho = (lowError - error) / denom
+        if rho < 0.25:
+            self.lambda_ *= boost
+        elif rho > 0.75:
+            self.lambda_ /= boost
+
+        ## update weights
+        self.net.param_buffer = weights - finalDW
+
+        return lowError
