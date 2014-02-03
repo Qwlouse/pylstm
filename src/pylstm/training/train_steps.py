35102b0a797334197d8f864dc2e251fa9632a46e
Klaus Greff <klaus@idsia.ch>
2014-02-03 15:16:53 +0100

    refactored train steps

--- src/pylstm/training/train_steps.py
+++ src/pylstm/training/train_steps.py
@@ -8,12 +8,31 @@ from pylstm.targets import Targets
 from pylstm.training.cg_trainer import conjgrad3
 
 
-class DiagnosticStep(object):
+class TrainingStep(object):
+    """
+    Base class for all training steps. Defines the common interface
+    """
     def __init__(self):
         self.net = None
 
     def start(self, net):
-        print("start DiagnosticStep with net=", net)
+        self.net = net
+        self._initialize()
+
+    def _initialize(self):
+        pass
+
+    def run(self, x, t, m):
+        pass
+
+
+class DiagnosticStep(TrainingStep):
+    """
+    Only prints debugging information. Does not train at all.
+    Use for diagnostics only.
+    """
+    def _initialize(self):
+        print("start DiagnosticStep with net=", self.net)
 
     def run(self, x, t, m):
         print("DiagnosticStep: x.shape=", x.shape)
@@ -25,28 +44,29 @@ class DiagnosticStep(object):
         return 15
 
 
-class ForwardStep(object):
-    def __init__(self):
-        self.net = None
-
-    def start(self, net):
-        self.net = net
-
+class ForwardStep(TrainingStep):
+    """
+    Only runs the forward pass and returns the error. Does not train the
+    network at all.
+    """
     def run(self, x, t, m):
         self.net.forward_pass(x)
         return self.net.calculate_error(t, m)
 
 
-class SgdStep(object):
+class SgdStep(TrainingStep):
+    """
+    Stochastic Gradient Descent.
+    """
     def __init__(self, learning_rate=0.1):
+        super(SgdStep, self).__init__()
         self.learning_rate = learning_rate
-        self.net = None
-
-    def start(self, net):
-        self.net = net
 
     def run(self, x, t, m):
-        learning_rate = self.learning_rate if isinstance(self.learning_rate, (int, float)) else self.learning_rate()
+        if isinstance(self.learning_rate, (int, float)):
+            learning_rate = self.learning_rate
+        else:
+            learning_rate = self.learning_rate()
         self.net.forward_pass(x)
         error = self.net.calculate_error(t, m)
         self.net.backward_pass(t, m)
@@ -55,20 +75,28 @@ class SgdStep(object):
         return error
 
 
-class MomentumStep(object):
+class MomentumStep(TrainingStep):
+    """
+    Stochastic Gradient Descent with a momentum term.
+    """
     def __init__(self, learning_rate=0.1, momentum=0.0):
+        super(MomentumStep, self).__init__()
         self.velocity = None
         self.momentum = momentum
         self.learning_rate = learning_rate
-        self.net = None
 
-    def start(self, net):
-        self.net = net
-        self.velocity = np.zeros(net.get_param_size())
+    def _initialize(self):
+        self.velocity = np.zeros(self.net.get_param_size())
 
     def run(self, x, t, m):
-        learning_rate = self.learning_rate if isinstance(self.learning_rate, (int, float)) else self.learning_rate()
-        momentum = self.momentum if isinstance(self.momentum, (int, float)) else self.momentum()
+        if isinstance(self.learning_rate, (int, float)):
+            learning_rate = self.learning_rate
+        else:
+            learning_rate = self.learning_rate()
+        if isinstance(self.momentum, (int, float)):
+            momentum = self.momentum
+        else:
+            momentum = self.momentum()
         self.velocity *= momentum
         self.net.forward_pass(x)
         error = self.net.calculate_error(t, m)
@@ -79,20 +107,28 @@ class MomentumStep(object):
         return error
 
 
-class NesterovStep(object):
+class NesterovStep(TrainingStep):
+    """
+    Stochastic Gradient Descent with a Nesterov-style momentum term.
+    """
     def __init__(self, learning_rate=0.1, momentum=0.0):
+        super(NesterovStep, self).__init__()
         self.velocity = None
         self.momentum = momentum
         self.learning_rate = learning_rate
-        self.net = None
 
-    def start(self, net):
-        self.net = net
-        self.velocity = np.zeros(net.get_param_size())
+    def _initialize(self):
+        self.velocity = np.zeros(self.net.get_param_size())
 
     def run(self, x, t, m):
-        learning_rate = self.learning_rate if isinstance(self.learning_rate, (int, float)) else self.learning_rate()
-        momentum = self.momentum if isinstance(self.momentum, (int, float)) else self.momentum()
+        if isinstance(self.learning_rate, (int, float)):
+            learning_rate = self.learning_rate
+        else:
+            learning_rate = self.learning_rate()
+        if isinstance(self.momentum, (int, float)):
+            momentum = self.momentum
+        else:
+            momentum = self.momentum()
         self.velocity *= momentum
         self.net.param_buffer += self.velocity
         self.net.forward_pass(x)
@@ -104,7 +140,7 @@ class NesterovStep(object):
         return error
 
 
-class RPropStep(object):
+class RPropStep(TrainingStep):
     """
     References:
     Improving the Rprop Learning Algorithm. Igel and Husken (2000).
@@ -114,8 +150,10 @@ class RPropStep(object):
     Rprop- can be obtained by setting backtracking = False
     iRprop+ can be obtained by setting backtracking = True but backtrack_on_error_drop = False
     """
-    def __init__(self, eta_minus=0.5, eta_plus=1.2, delta_0=0.1, delta_min=1e-6, delta_max=50,
-                 backtracking=True, backtrack_on_error_drop=True):
+
+    def __init__(self, eta_minus=0.5, eta_plus=1.2, delta_0=0.1, delta_min=1e-6,
+                 delta_max=50, backtracking=True, backtrack_on_error_drop=True):
+        super(RPropStep, self).__init__()
         self.eta_plus = eta_plus
         self.eta_minus = eta_minus
         self.delta = delta_0
@@ -124,9 +162,11 @@ class RPropStep(object):
         self.initialized = False
         self.backtracking = backtracking
         self.backtrack_on_error_drop = backtrack_on_error_drop
+        self.last_grad_sign = 0
+        self.last_update = 0
+        self.last_error = np.Inf
 
-    def start(self, net):
-        self.net = net
+    def _initialize(self):
         self.last_grad_sign = 0
         self.last_update = 0
         self.last_error = np.Inf
@@ -154,7 +194,8 @@ class RPropStep(object):
                          (-self.last_update * (sign_flip < 0))
             else:
                 update = (-np.sign(grad) * self.delta * (sign_flip >= 0)) + \
-                         (-self.last_update * (sign_flip < 0) * (error > self.last_error))
+                         (-self.last_update * (sign_flip < 0) *
+                          (error > self.last_error))
         else:
             update = -np.sign(grad) * self.delta
 
@@ -170,19 +211,20 @@ class RPropStep(object):
         return error
 
 
-class RmsPropStep(object):
-    def __init__(self, step_rate=0.1, decay=0.9, momentum=0.0,
-                 step_adapt=False, step_rate_min=0, step_rate_max=np.inf):
+class RmsPropStep(TrainingStep):
+    def __init__(self, step_rate=0.1, decay=0.9, momentum=0.0, step_adapt=False,
+                 step_rate_min=0, step_rate_max=np.inf):
+        super(RmsPropStep, self).__init__()
         self.step_rate = step_rate
         self.decay = decay
         self.momentum = momentum
         self.step_adapt = step_adapt
         self.step_rate_min = step_rate_min
         self.step_rate_max = step_rate_max
+        self.scaling_factor = None
 
-    def start(self, net):
-        self.net = net
-        self.scaling_factor = np.zeros(net.get_param_size())
+    def _initialize(self):
+        self.scaling_factor = np.zeros(self.net.get_param_size())
 
     def run(self, x, t, m):
         self.net.forward_pass(x)
@@ -190,23 +232,24 @@ class RmsPropStep(object):
         self.net.backward_pass(t, m)
         grad = self.net.calc_gradient()
 
-        self.scaling_factor = (1 - self.decay) * grad**2 + self.decay * self.scaling_factor
+        self.scaling_factor = (1 - self.decay) * grad**2 + self.decay * \
+                              self.scaling_factor
         update = (self.step_rate / self.scaling_factor) * grad
-        #self.net.param_buffer += update.flatten()
-
+        self.net.param_buffer += update.flatten()
         return error
 
 
-class CgStep(object):
-    def __init__(self, minibatch_size=32, mu=1./30, maxiter=300):
+class CgStep(TrainingStep):
+    def __init__(self, minibatch_size=32, mu=1. / 30, maxiter=300):
+        super(CgStep, self).__init__()
         self.minibatch_size = minibatch_size
         self.mu = mu
         self.lambda_ = 0.1
         self.maxiter = maxiter
         self.net = None
 
-    def start(self, net):
-        self.net = net
+    def _initialize(self):
+        self.lambda_ = 0.1
 
     def run(self, X, T, M):
         mb = Minibatches(X, T, M, self.minibatch_size, verbose=False)()
