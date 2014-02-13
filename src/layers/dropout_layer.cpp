42ea8043cd4330f0789ff153a5cba33a406e1a11
Rupesh Kumar Srivastava <rupspace@gmail.com>
2014-02-13 18:02:54 +0100

    DropoutLayer WIP: works with training_pass flag.

--- src/layers/dropout_layer.cpp
+++ src/layers/dropout_layer.cpp
@@ -36,22 +36,32 @@ DropoutLayer::FwdState::FwdState(size_t n_inputs, size_t, size_t n_batches, size
 
 ////////////////////// Bwd Buffer /////////////////////////////////////////////
 
-DropoutLayer::BwdState::BwdState(size_t n_inputs, size_t, size_t n_batches, size_t time)
+DropoutLayer::BwdState::BwdState(size_t, size_t, size_t, size_t)
 {
 }
 
 ////////////////////// Methods /////////////////////////////////////////////
-void DropoutLayer::forward(DropoutLayer::Parameters &, DropoutLayer::FwdState &b, Matrix &x, Matrix &y) {
-    for (size_t i = 0; i < b.Mask.n_rows; i++) {
-        for (size_t j = 0; j < b.Mask.n_columns; j++) {
-            for (size_t k = 0; k < b.Mask.n_slices; k++) {
-                double prob = (rand_r(&rnd_state) / (double(RAND_MAX) + 1.0));
-                b.Mask.get(i, j, k) = prob > drop_prob ? 1 : 0;
-                // Optimize by taking dot here?
+void DropoutLayer::forward(DropoutLayer::Parameters &, DropoutLayer::FwdState &b, Matrix &x, Matrix &y, bool training_pass) {
+    if (training_pass) {
+        for (size_t i = 0; i < b.Mask.n_rows; i++) {
+            for (size_t j = 0; j < b.Mask.n_columns; j++) {
+                for (size_t k = 0; k < b.Mask.n_slices; k++) {
+                    double prob = (rand_r(&rnd_state) / (double(RAND_MAX) + 1.0));
+                    b.Mask.get(i, j, k) = prob > drop_prob ? 1 : 0;
+                    y.get(i, j, k) = b.Mask.get(i, j, k) * x.get(i, j, k);
+                }
             }
         }
     }
-	dot(b.Mask, x.flatten_time(), y.flatten_time());
+    else {
+        for (size_t i = 0; i < b.Mask.n_rows; i++) {
+            for (size_t j = 0; j < b.Mask.n_columns; j++) {
+                for (size_t k = 0; k < b.Mask.n_slices; k++) {
+                    y.get(i, j, k) = (1 - drop_prob) * x.get(i, j, k);
+                }
+            }
+        }
+    }
 }
 
 void DropoutLayer::backward(DropoutLayer::Parameters &, DropoutLayer::FwdState &b, DropoutLayer::BwdState &, Matrix &, Matrix &in_deltas, Matrix &out_deltas) {
@@ -62,10 +72,10 @@ void DropoutLayer::gradient(DropoutLayer::Parameters&, DropoutLayer::Parameters&
 {
 }
 
-void DropoutLayer::Rpass(Parameters &w, Parameters &v,  FwdState &, FwdState &Rb, Matrix &x, Matrix &y, Matrix& Rx, Matrix &Ry)
+void DropoutLayer::Rpass(Parameters &, Parameters &,  FwdState &, FwdState &, Matrix &, Matrix &, Matrix&, Matrix &)
 {
 }
 
-void DropoutLayer::dampened_backward(Parameters& w, FwdState& b, BwdState& d, Matrix& y, Matrix& in_deltas, Matrix& out_deltas, FwdState&, double, double)
+void DropoutLayer::dampened_backward(Parameters&, FwdState&, BwdState&, Matrix&, Matrix&, Matrix&, FwdState&, double, double)
 {
 }
