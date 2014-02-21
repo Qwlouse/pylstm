eefb4182fedb0768ea69377f0a1de24d8046ccf1
Qwlouse <klaus.greff@gmx.net>
2014-02-21 02:16:39 +0100

    reformatted file

--- test/pylstm/test_buffer_manager.py
+++ test/pylstm/test_buffer_manager.py
@@ -56,19 +56,18 @@ class BufferConstructionTest(unittest.TestCase):
         ]
 
     def test_get_forward_closure(self):
-        for spec, sources_expected, sinks_expected in self.forward_closure_tests:
+        for spec, sources_exp, sinks_exp in self.forward_closure_tests:
             architecture = generate_architecture(spec)
             sources, sinks = get_forward_closure('A', architecture)
-            self.assertSetEqual(sources, sources_expected)
-            self.assertSetEqual(sinks, sinks_expected)
+            self.assertSetEqual(sources, sources_exp)
+            self.assertSetEqual(sinks, sinks_exp)
 
 
 class BufferManagerTest(unittest.TestCase):
-
     def test_calculate_size_with_one_source_size_getter(self):
         bm = BufferManager()
-        size_getter = lambda t, b : 10 * t + b
-        view_factory = lambda x, t, b : x
+        size_getter = lambda t, b: 10 * t + b
+        view_factory = lambda x, t, b: x
         bm.add({'foo': (size_getter, view_factory)}, {})
         bm.set_dimensions(1, 1)
         self.assertEqual(bm.calculate_size(), 11)
@@ -79,8 +78,8 @@ class BufferManagerTest(unittest.TestCase):
 
     def test_calculate_size_with_one_sink_size_getter(self):
         bm = BufferManager()
-        size_getter = lambda t, b : 10 * t + b
-        view_factory = lambda x, t, b : x
+        size_getter = lambda t, b: 10 * t + b
+        view_factory = lambda x, t, b: x
         bm.add({}, {'foo': (size_getter, view_factory)})
         bm.set_dimensions(1, 1)
         self.assertEqual(bm.calculate_size(), 11)
@@ -91,9 +90,10 @@ class BufferManagerTest(unittest.TestCase):
 
     def test_calculate_size_with_one_sink_and_one_source_size_getter(self):
         bm = BufferManager()
-        size_getter = lambda t, b : 10 * t + b
-        view_factory = lambda x, t, b : x
-        bm.add({'source': (size_getter, view_factory)}, {'sink': (size_getter, view_factory)})
+        size_getter = lambda t, b: 10 * t + b
+        view_factory = lambda x, t, b: x
+        bm.add({'source': (size_getter, view_factory)},
+               {'sink': (size_getter, view_factory)})
         bm.set_dimensions(1, 1)
         self.assertEqual(bm.calculate_size(), 11)
         bm.set_dimensions(10, 5)
@@ -103,10 +103,10 @@ class BufferManagerTest(unittest.TestCase):
 
     def test_calculate_size_with_multiple_size_getters(self):
         bm = BufferManager()
-        sg1 = lambda t, b : 10*t + b
-        sg2 = lambda t, b : 1000*t + 100*b
-        sg3 = lambda t, b : 100000*t + 10000*b
-        view_factory = lambda x, t, b : x
+        sg1 = lambda t, b: 10 * t + b
+        sg2 = lambda t, b: 1000 * t + 100 * b
+        sg3 = lambda t, b: 100000 * t + 10000 * b
+        view_factory = lambda x, t, b: x
         bm.add({'foo1': (sg1, view_factory)}, {})
         bm.add({'foo2': (sg2, view_factory)}, {})
         bm.add({}, {'foo3': (sg3, view_factory)})
@@ -117,9 +117,9 @@ class BufferManagerTest(unittest.TestCase):
 
     def test_get_buffer_has_right_size(self):
         bm = BufferManager()
-        sg1 = lambda t, b : 2 * t * b
-        sg2 = lambda t, b : 3 * t * b
-        view_factory = lambda x, t, b : x
+        sg1 = lambda t, b: 2 * t * b
+        sg2 = lambda t, b: 3 * t * b
+        view_factory = lambda x, t, b: x
         bm.add({'foo1': (sg1, view_factory), 'foo2': (sg2, view_factory)}, {})
         bm.set_dimensions(1, 1)
         self.assertEqual(len(bm.get_source_view('foo1')), 2)
@@ -127,6 +127,7 @@ class BufferManagerTest(unittest.TestCase):
         bm.set_dimensions(2, 3)
         self.assertEqual(len(bm.get_source_view('foo1')), 12)
         self.assertEqual(len(bm.get_source_view('foo2')), 18)
+
 
 if __name__ == "__main__":
     unittest.main()
