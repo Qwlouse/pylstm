07be684ddf6afba47f094b9e8524eb7acef13cc4
Klaus Greff <klaus@idsia.ch>
2014-02-25 16:58:28 +0100

    refactored targets and added trim() function

--- src/pylstm/targets.py
+++ src/pylstm/targets.py
@@ -33,14 +33,29 @@ class Targets(object):
     Baseclass for all targets objects. A targets object holds all the desired
     outputs and the corresponding mask (if any).
     """
-    def __init__(self, targets_type, binarizing):
+    def __init__(self, targets_type, binarizing, mask):
         self.targets_type = (targets_type, binarizing)
+        self.mask = None
+        if mask is not None:
+            assert (mask.ndim == 3 and mask.shape[2] == 1) or (mask.ndim == 2)
+            self.mask = mask.reshape(mask.shape[0], mask.shape[1], 1)
 
     def __getitem__(self, item):
         raise NotImplementedError()
 
-    def crop_time(self, end):
-        raise NotImplementedError()
+    def trim(self):
+        """
+        Cut all the timesteps from the end of the sequence that have a mask
+        value of 0.
+        @return: The number of trimmed timesteps
+        @rtype: int
+        """
+        if self.mask is None:
+            return 0
+        # get trim amount
+        k = np.min(self.mask[::-1, :, 0].argmax(axis=0))
+        self.mask = self.mask[:-k, :, :]
+        return k
 
     def validate_for_output_shape(self, timesteps, batchsize, out_size):
         raise NotImplementedError()
@@ -59,27 +74,26 @@ class FramewiseTargets(Targets):
     might be masked out.
     """
     def __init__(self, targets, mask=None, binarize_to=None):
-        super(FramewiseTargets, self).__init__('F', binarize_to is not None)
+        super(FramewiseTargets, self).__init__('F', binarize_to is not None,
+                                               mask)
         assert (targets.ndim == 3) or (binarize_to and targets.ndim == 2)
         self.binarize_to = binarize_to
         self.data = targets.reshape(targets.shape[0], targets.shape[1], -1)
         if binarize_to:
             self.data = np.array(self.data, dtype=np.int)
-        self.mask = None
-        if mask is not None:
-            assert (mask.ndim == 3 and mask.shape[2] == 1) or (mask.ndim == 2)
-            self.mask = mask.reshape(mask.shape[0], mask.shape[1], 1)
-            assert_shape_equals(self.data, self.mask)
+        if self.mask is not None:
+            assert self.mask.shape[0] == self.data.shape[0]
+            assert self.mask.shape[1] == self.data.shape[1]
 
     def __getitem__(self, item):
         t = self.data[:, item, :]
         m = self.mask[:, item, :] if self.mask is not None else None
         return FramewiseTargets(t, m, binarize_to=self.binarize_to)
 
-    def crop_time(self, end):
-        t = self.data[:end, :, :]
-        m = self.mask[:end, :, :] if self.mask is not None else None
-        return FramewiseTargets(t, m, binarize_to=self.binarize_to)
+    def trim(self):
+        k = Targets.trim(self)
+        self.data = self.data[:-k, :, :]
+        return k
 
     def validate_for_output_shape(self, timesteps, batchsize, out_size):
         if self.binarize_to:
@@ -99,13 +113,10 @@ class LabelingTargets(Targets):
     resulting deltas will be masked.
     """
     def __init__(self, labels, mask=None, binarize_to=None):
-        super(LabelingTargets, self).__init__('L', binarize_to is not None)
+        super(LabelingTargets, self).__init__('L', binarize_to is not None,
+                                              mask)
         self.data = labels
         self.binarize_to = binarize_to
-        self.mask = None
-        if mask is not None:
-            assert (mask.ndim == 3 and mask.shape[2] == 1) or (mask.ndim == 2)
-            self.mask = mask.reshape(mask.shape[0], mask.shape[1], 1)
 
     def __getitem__(self, item):
         if isinstance(item, slice) or isinstance(item, int):
@@ -118,11 +129,6 @@ class LabelingTargets(Targets):
             return LabelingTargets(l, m, binarize_to=self.binarize_to)
         else:
             raise ValueError("Indexing with type '%s' unsupported" % type(item))
-
-    def crop_time(self, end):
-        t = self.data
-        m = self.mask[:end, :, :] if self.mask is not None else None
-        return LabelingTargets(t, m, binarize_to=self.binarize_to)
 
     def validate_for_output_shape(self, timesteps, batchsize, out_size):
         assert len(self.data) == batchsize
@@ -142,26 +148,17 @@ class SequencewiseTargets(Targets):
     timesteps will receive deltas.
     """
     def __init__(self, sequence_targets, mask=None, binarize_to=None):
-        super(SequencewiseTargets, self).__init__('S', binarize_to is not None)
+        super(SequencewiseTargets, self).__init__('S', binarize_to is not None,
+                                                  mask)
         assert sequence_targets.ndim == 2 or \
             (binarize_to and sequence_targets.ndim == 1)
         self.binarize_to = binarize_to
         self.data = sequence_targets.reshape(sequence_targets.shape[0], -1)
-        self.mask = None
-        if mask is not None:
-            assert (mask.ndim == 3 and mask.shape[2] == 1) or (mask.ndim == 2)
-            assert sequence_targets.shape[1] == mask.shape[1]
-            self.mask = mask.reshape(mask.shape[0], mask.shape[1], 1)
 
     def __getitem__(self, item):
         s = self.data[item]
         m = self.mask[:, item, :] if self.mask is not None else None
         return SequencewiseTargets(s, m, binarize_to=self.binarize_to)
-
-    def crop_time(self, end):
-        s = self.data
-        m = self.mask[:end, :, :] if self.mask is not None else None
-        return SequencewiseTargets(s, m, self.binarize_to)
 
     def validate_for_output_shape(self, timesteps, batchsize, out_size):
         if self.binarize_to:
