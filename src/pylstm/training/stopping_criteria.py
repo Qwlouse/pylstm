5e54ac1789453d12fbf0900a0d72e80831128ae5
Qwlouse <klaus.greff@gmx.net>
2014-03-17 19:10:04 +0100

    converted stopping criteria to describable

--- src/pylstm/training/stopping_criteria.py
+++ src/pylstm/training/stopping_criteria.py
@@ -3,18 +3,24 @@
 
 from __future__ import division, print_function, unicode_literals
 import numpy as np
+from pylstm.describable import Describable
 
 
-class ValidationErrorRises(object):
-    def __init__(self, delay=1):
-        self.delay = delay
+class StoppingCriterion(Describable):
+    def __call__(self, epochs_seen, net, training_errors, validation_errors):
+        raise NotImplementedError()
 
     def restart(self):
         pass
 
+
+class ValidationErrorRises(StoppingCriterion):
+    def __init__(self, delay=1):
+        self.delay = delay
+
     def __call__(self, epochs_seen, net, training_errors, validation_errors):
-        assert len(validation_errors), "You need to specify a validation set " \
-                                       "if you want to use ValidationErrorRises"
+        assert validation_errors, "You need to specify a validation set " \
+                                  "if you want to use ValidationErrorRises"
         best_val_error = np.argmin(validation_errors)
         if len(validation_errors) > best_val_error + self.delay:
             print("Validation error did not fall for %d epochs! Stopping."
@@ -23,12 +29,9 @@ class ValidationErrorRises(object):
         return False
 
 
-class MaxEpochsSeen(object):
+class MaxEpochsSeen(StoppingCriterion):
     def __init__(self, max_epochs=100):
         self.max_epochs = max_epochs
-
-    def restart(self):
-        pass
 
     def __call__(self, epochs_seen, net, training_errors, validation_errors):
         if epochs_seen >= self.max_epochs:
