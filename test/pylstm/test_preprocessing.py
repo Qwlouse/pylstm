03d2ab7a30da5d561cf026563069ae87146127e8
Klaus Greff <klaus@idsia.ch>
2014-04-14 18:03:44 +0200

    added test for center_dataset

--- test/pylstm/test_preprocessing.py
+++ test/pylstm/test_preprocessing.py
@@ -5,13 +5,14 @@ from __future__ import division, print_function, unicode_literals
 import unittest
 import numpy as np
 from numpy.testing import *
+from pylstm import FramewiseTargets
 from pylstm.datasets.preprocessing import (get_means, subtract_means, get_stds,
-                                           divide_by_stds)
+                                           divide_by_stds, center_dataset)
 
 
 class PreprocessingTests(unittest.TestCase):
     def setUp(self):
-        self.X = np.arange(24, dtype=np.float64).reshape(3, 4, 2)
+        self.X = np.arange(24, dtype=np.float64).reshape((3, 4, 2))
         self.M = np.ones((3, 4, 1))
         self.M[2, 0, 0] = 0
         self.M[1:, 1, 0] = 0
@@ -112,4 +113,37 @@ class PreprocessingTests(unittest.TestCase):
         stds = get_stds(self.X, mask=self.M)
         assert_allclose(stds, [6.27992834, 1.0])
 
+    #################### center_dataset ####################
 
+    def test_center_dataset(self):
+        ds = {
+            'training': (self.X, FramewiseTargets(self.X)),
+            'validation': (self.X + 1, FramewiseTargets(self.X)),
+            'test': (self.X * 2, FramewiseTargets(self.X))
+        }
+
+        assert_allclose(get_means(ds['training'][0]), [11., 12.])
+        assert_allclose(get_means(ds['validation'][0]), [12., 13.])
+        assert_allclose(get_means(ds['test'][0]), [22., 24.])
+
+        center_dataset(ds)
+
+        assert_allclose(get_means(ds['training'][0]), [0., 0.], atol=1e-6)
+        assert_allclose(get_means(ds['validation'][0]), [1., 1.], atol=1e-6)
+        assert_allclose(get_means(ds['test'][0]), [11., 12.], atol=1e-6)
+
+    def test_center_dataset_masked(self):
+        ds = {
+            'training': (self.X, FramewiseTargets(self.X, self.M)),
+            'validation': (self.X + 1, FramewiseTargets(self.X, self.M)),
+            'test': (self.X * 2, FramewiseTargets(self.X, self.M))
+        }
+        assert_allclose(get_means(ds['training'][0], self.M), [8.25, 9.25])
+        assert_allclose(get_means(ds['validation'][0], self.M), [9.25, 10.25])
+        assert_allclose(get_means(ds['test'][0], self.M), [16.5, 18.5])
+
+        center_dataset(ds)
+
+        assert_allclose(get_means(ds['training'][0], self.M), [0., 0.])
+        assert_allclose(get_means(ds['validation'][0], self.M), [1., 1.])
+        assert_allclose(get_means(ds['test'][0], self.M), [8.25, 9.25])
